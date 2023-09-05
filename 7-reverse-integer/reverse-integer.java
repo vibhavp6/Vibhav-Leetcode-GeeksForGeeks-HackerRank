2a77d6c1f60dbfd64c9d6945df0597aae06a5eb0
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2023-09-05 15:03:46 +0530

    Time: 1 ms (98.32%) | Memory: 39.2 MB (94.70%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -5,7 +5,8 @@ class Solution {
             rev = rev*10+(x%10);
             x = x/ 10;
         }
-        if(rev>Math.pow(2,31) || rev<Math.pow(-2,31)) return 0;
-        return (int)rev;
+        if(rev>Math.pow(2,31) || rev<Math.pow(-2,31))
+        return 0;
+        return (int) rev;
     }
 }
