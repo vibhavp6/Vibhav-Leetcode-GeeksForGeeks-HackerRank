c246c1b1ee97afdcf782d9ac09fdb6c807052226
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-01-24 23:42:52 +0530

    Time: 39 ms (26.69%) | Memory: 61.7 MB (99.29%) - LeetSync

---
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+import java.util.Stack;
+
+class Solution {
+    public int pseudoPalindromicPaths(TreeNode root) {
+        int count = 0, path = 0;
+
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
+        stack.push(new Pair<>(root, 0));
+
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> p = stack.pop();
+            TreeNode node = p.getKey();
+            path = p.getValue();
+
+            if (node != null) {
+                path = path ^ (1 << node.val);
+
+                if (node.left == null && node.right == null) {
+                    if ((path & (path - 1)) == 0) {
+                        ++count;
+                    }
+                } else {
+                    stack.push(new Pair<>(node.right, path));
+                    stack.push(new Pair<>(node.left, path));
+                }
+            }
+        }
+
+        return count;
+    }
+}
