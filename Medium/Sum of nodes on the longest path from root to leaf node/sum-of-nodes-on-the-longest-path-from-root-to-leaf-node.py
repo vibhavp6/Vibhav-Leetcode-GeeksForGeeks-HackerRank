ac3cba4ad35a7e6e8c6429d5390686df46aface4
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-03-20 09:04:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of nodes on the longest path from root to leaf node/sum-of-nodes-on-the-longest-path-from-root-to-leaf-node.py
@@ -0,0 +1,104 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self,val):
+        self.data=val
+        self.left=None
+        self.right=None
+'''
+class Solution:
+    def sumOfLongRootToLeafPath(self,root):
+        #code here
+        res=0
+        temp=[(root,root.data)]
+        while temp:
+            t_max=0
+            t=[]
+            for i in range(len(temp)):
+                cur,data=temp[i]
+                t_max=max(t_max,data)
+                if cur.left:
+                    t.append((cur.left,data+cur.left.data))
+                if cur.right:
+                    t.append((cur.right, data+ cur.right.data))
+            temp=t
+            res=t_max
+        return res
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+from collections import deque
+class Node:
+    def __init__(self,val):
+        self.data=val
+        self.left=None
+        self.right=None
+        
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        s=input()
+        root=buildTree(s)
+        ob = Solution()
+        res = ob.sumOfLongRootToLeafPath(root)
+        print(res)
+# } Driver Code Ends
