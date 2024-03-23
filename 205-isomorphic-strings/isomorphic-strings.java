3c5c2306c4204f65af5fac1bdb3437fdb6432e2c
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-03-23 19:57:30 +0530

    Time: 6 ms (94.08%) | Memory: 42.5 MB (38.75%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        
+        int[] sMap = new int[256];
+        int[] tMap = new int[256];
+        
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            
+            if (sMap[sChar] != tMap[tChar])
+                return false;
+            
+            sMap[sChar] = i + 1;
+            tMap[tChar] = i + 1;
+        }
+        
+        return true;
+    }
+}
