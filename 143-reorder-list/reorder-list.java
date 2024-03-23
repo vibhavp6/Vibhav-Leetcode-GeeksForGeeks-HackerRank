e0f717dc01f298b1fd4fd8cebd2250d9461ff7f6
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-03-23 16:16:07 +0530

    Time: 2 ms (88.69%) | Memory: 48 MB (64.83%) - LeetSync

---
+++ 143-reorder-list/reorder-list.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/*class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}*/
+
+class Solution {
+    public ListNode reverse(ListNode head) {
+        if (head == null) return null;
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode nextNode = null;
+        while (curr != null) {
+            nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+    public void merge(ListNode list1, ListNode list2) {
+        while (list2 != null) {
+            ListNode nextNode = list1.next;
+            list1.next = list2;
+            list1 = list2;
+            list2 = nextNode;
+        }
+    }
+
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        prev.next = null;
+        ListNode list1 = head;
+        ListNode list2 = reverse(slow);
+        merge(list1, list2);
+    }
+}
