06636727184d8f86ac25ef1a35b39af1c285d6f6
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-04-15 21:41:25 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count the elements/count-the-elements.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> countElements(vector<int> &a, vector<int> &b, int n, vector<int> &query,
+                              int q) {
+        sort(b.begin(), b.end());
+        vector<int> ans;
+        
+        for(int i : query) {
+            int index = upper_bound(b.begin(), b.end(), a[i]) - b.begin();
+            ans.push_back(index);
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+
+        int n;
+        cin >> n;
+        vector<int> a, b, ans;
+        int input;
+        for (int i = 0; i < n; i++) {
+            cin >> input;
+            a.push_back(input);
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> input;
+            b.push_back(input);
+        }
+        int q;
+        cin >> q;
+        vector<int> query;
+        for (int i = 0; i < q; i++) {
+            cin >> input;
+            query.push_back(input);
+        }
+        Solution obj;
+        ans = obj.countElements(a, b, n, query, q);
+        for (int i = 0; i < q; i++) {
+            cout << ans[i] << endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
