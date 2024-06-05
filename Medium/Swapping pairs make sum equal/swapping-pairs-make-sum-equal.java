7cb8414322b188972f34e10e33a6bc775726a501
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-06-05 09:45:38 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Swapping pairs make sum equal/swapping-pairs-make-sum-equal.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    long findSwapValues(long a[], int n, long b[], int m) {
+        // Your code goes here
+         long suma = 0;
+        for (long num : a) {
+            suma += num;
+        }
+        
+        long sumb = 0;
+        for (long num : b) {
+            sumb += num;
+        }
+        
+        long diff = suma - sumb;
+        if (diff % 2 != 0) {
+            return -1;
+        }
+        diff /= 2;
+
+        HashSet<Long> set = new HashSet<>();
+        for (long num : a) {
+            set.add(num);
+        }
+        
+        for (long num : b) {
+            if (set.contains(num + diff)) {
+                return 1;
+            }
+        }
+        return -1;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+            String line = br.readLine();
+            String[] q = line.trim().split("\\s+");
+            int n = Integer.parseInt(q[0]);
+            int m = Integer.parseInt(q[1]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(a1[i]);
+            }
+            String line2 = br.readLine();
+            String[] a2 = line2.trim().split("\\s+");
+            long b[] = new long[m];
+            for (int i = 0; i < m; i++) {
+                b[i] = Long.parseLong(a2[i]);
+            }
+            Solution ob = new Solution();
+            long ans = ob.findSwapValues(a, n, b, m);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
