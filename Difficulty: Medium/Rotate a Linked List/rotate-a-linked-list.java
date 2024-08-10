25e7e79053a6a676eb9d63106567eecfdcbf977f
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-08-10 15:00:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate a Linked List/rotate-a-linked-list.java
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+
+// } Driver Code Ends
+/* node of linked list:
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+
+*/
+
+class Solution {
+    // Function to rotate a linked list.
+    public Node rotate(Node head, int k) {
+        // add code here
+        if(head == null && head.next == null){
+            return head;
+        }
+        int size =0;
+        Node temp = head;
+        Node last = null;
+        while(temp != null){
+            size++;
+            last = temp;
+            temp = temp.next;
+        }
+        
+        Node curr = head;
+        Node next = head.next;
+        while(k > 0){
+            curr.next = null;
+            last.next = curr;
+            last = last.next;
+            curr = next;
+            next = curr.next;
+            k--;
+        }
+        return curr;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+public class GFG {
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = br.readLine().trim();
+            if (!input.isEmpty()) {
+                String[] numbers = input.split("\\s+");
+                for (String num : numbers) {
+                    if (!num.isEmpty()) {
+                        arr.add(Integer.parseInt(num));
+                    }
+                }
+            }
+
+            Node head = null;
+            if (!arr.isEmpty()) {
+                head = new Node(arr.get(0));
+                Node tail = head;
+                for (int i = 1; i < arr.size(); ++i) {
+                    tail.next = new Node(arr.get(i));
+                    tail = tail.next;
+                }
+            }
+            int k = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            head = ob.rotate(head, k);
+            printList(head);
+        }
+    }
+}
+
+// } Driver Code Ends
