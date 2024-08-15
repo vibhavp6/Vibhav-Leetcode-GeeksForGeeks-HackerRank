4ac67b42cc3c597f48825301aeb328835c7c04bd
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-08-15 12:58:48 +0530

    Time: 1 ms (100.00%) | Memory: 56.4 MB (10.54%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five_dollars = 0, ten_dollars = 0;
+
+        for (int x : bills) {
+            if (x == 5) {
+                five_dollars++;
+            } else if (x == 10) {
+                if (five_dollars > 0) {
+                    five_dollars--;
+                    ten_dollars++;
+                } else {
+                    return false;
+                }
+            } else {
+                if (five_dollars > 0 && ten_dollars > 0) {
+                    five_dollars--;
+                    ten_dollars--;
+                } else if (five_dollars > 2) {
+                    five_dollars -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
