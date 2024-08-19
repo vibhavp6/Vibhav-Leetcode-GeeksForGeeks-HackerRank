ec6d5a46289712127e1ea9e5799afb9ba3c72ac0
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-08-19 13:12:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int key = Integer.parseInt(in.readLine().trim());
+            Solution ob = new Solution();
+            out.println(ob.kthSmallest(arr, key));
+        }
+        out.flush();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int kthSmallest(int[] arr, int k) {
+        // Your code here
+         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i : arr){
+            pq.add(i);
+        }
+        while(k!=1){
+            k--;
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
