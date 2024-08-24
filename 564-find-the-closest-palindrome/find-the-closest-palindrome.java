86ad85237a4ef5dd5468ad4da3550d8473336344
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-08-24 13:27:07 +0530

    Time: 1 ms (100.00%) | Memory: 41.7 MB (66.00%) - LeetSync

---
+++ 564-find-the-closest-palindrome/find-the-closest-palindrome.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String nearestPalindromic(String numberStr) {
+        long number = Long.parseLong(numberStr);
+        if (number <= 10) return String.valueOf(number - 1);
+        if (number == 11) return "9";
+
+        int length = numberStr.length();
+        long leftHalf = Long.parseLong(numberStr.substring(0, (length + 1) / 2));
+        
+        long[] palindromeCandidates = new long[5];
+        palindromeCandidates[0] = generatePalindromeFromLeft(leftHalf - 1, length % 2 == 0);
+        palindromeCandidates[1] = generatePalindromeFromLeft(leftHalf, length % 2 == 0);
+        palindromeCandidates[2] = generatePalindromeFromLeft(leftHalf + 1, length % 2 == 0);
+        palindromeCandidates[3] = (long)Math.pow(10, length - 1) - 1;
+        palindromeCandidates[4] = (long)Math.pow(10, length) + 1;
+
+        long nearestPalindrome = 0;
+        long minDifference = Long.MAX_VALUE;
+
+        for (long candidate : palindromeCandidates) {
+            if (candidate == number) continue;
+            long difference = Math.abs(candidate - number);
+            if (difference < minDifference || (difference == minDifference && candidate < nearestPalindrome)) {
+                minDifference = difference;
+                nearestPalindrome = candidate;
+            }
+        }
+
+        return String.valueOf(nearestPalindrome);
+    }
+
+    private long generatePalindromeFromLeft(long leftHalf, boolean isEvenLength) {
+        long palindrome = leftHalf;
+        if (!isEvenLength) leftHalf /= 10;
+        while (leftHalf > 0) {
+            palindrome = palindrome * 10 + leftHalf % 10;
+            leftHalf /= 10;
+        }
+        return palindrome;
+    }
+}
+
+//https://leetcode.com/problems/find-the-closest-palindrome/submissions/1364740180/
