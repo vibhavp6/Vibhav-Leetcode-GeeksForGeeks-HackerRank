83d05319feaed033ae75d769e5f3775cb78f126a
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-09-04 19:26:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count ways to N'th Stair(Order does not matter)/count-ways-to-nth-stair-order-does-not-matter.java
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            long ans = ob.nthStair(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int nthStair(int n) {
+        // Code here
+         return n/2 +1 ;
+    }
+}
