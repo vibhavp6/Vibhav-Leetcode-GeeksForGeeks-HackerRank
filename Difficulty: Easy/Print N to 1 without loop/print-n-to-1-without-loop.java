6fd4df0d2e64eab08968f202f298254556c1daed
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-09-05 16:00:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print N to 1 without loop/print-n-to-1-without-loop.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int N = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printNos(N);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printNos(int n) {
+        // code here
+        
+        if (n == 0) return;
+        
+        System.out.print(n + " ");
+        printNos(n-1);
+        
+    }
+}
