4bcd635b88ca9f644f83d720da5013fdcf5aa135
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-09-06 19:17:53 +0530

    Time: 1 ms (95.52%) | Memory: 42.6 MB (88.74%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,20 +1,31 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        long max1=Long.MIN_VALUE;
-        long max2=Long.MIN_VALUE;
-        long max3=Long.MIN_VALUE;
-        for(int num : nums){
-            if(num>max1){
+        long max1 = Long.MIN_VALUE;
+        long max2 = Long.MIN_VALUE;
+        long max3 = Long.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num == max1 || num == max2 || num == max3) {
+                continue;
+            }
+
+            if (num > max1) {
                 max3 = max2;
                 max2 = max1;
                 max1 = num;
-            }else if(max1>num && num>max2){
+            } else if (num > max2) {
                 max3 = max2;
                 max2 = num;
-            }else if(max2>num && num>max3){
-                max3=num;
+            } else if (num > max3) {
+                max3 = num;
             }
         }
-        return max3 != Long.MIN_VALUE ? (int) max3 : (int) max1;
+
+        // Return using an if-else statement.
+        if (max3 > Long.MIN_VALUE) {
+            return (int) max3;
+        } else {
+            return (int) max1;
+        }
     }
 }
