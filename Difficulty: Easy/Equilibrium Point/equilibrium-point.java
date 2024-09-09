62d0dc26324537e5a8b350745ed23b89b3567757
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-09-09 18:21:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Long> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Long.parseLong(token));
+            }
+
+            long[] arr = new long[array.size()];
+            int idx = 0;
+            for (long i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.equilibriumPoint(arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // arr: input array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long arr[]) {
+        // Your code here
+        int n =arr.length;
+        long sum = 0;
+        long leftsum =0;
+        long temp = -1;
+        
+        for (int i = 0 ; i<n; i++) {
+            sum += arr[i];
+        }
+        
+        if (n==1) return 1;
+        
+        for (int i =0; i<n; i++) {
+            sum -= arr[i];
+            if (sum == leftsum) {
+                temp =i+1;
+            }
+            
+            leftsum += arr[i];
+        }
+        
+        return (int) temp ;
+    }
+}
