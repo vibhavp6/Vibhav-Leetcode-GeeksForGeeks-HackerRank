d5d4a4de7e6003c796ec62e7931ad02444fac62e
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-09-09 20:48:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of Two Sorted Arrays/union-of-two-sorted-arrays.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t  =Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String st[] = read.readLine().trim().split(" ");
+            int N = Integer.parseInt(st[0]);
+            int M = Integer.parseInt(st[1]);
+            
+            int arr1[] = new int[N];
+            int arr2[] = new int[M];
+            
+            st = read.readLine().trim().split(" ");
+            for(int i = 0; i < N; i++)
+              arr1[i] = Integer.parseInt(st[i]);
+            
+            st = read.readLine().trim().split(" ");  
+            for(int i = 0; i< M; i++)
+              arr2[i] = Integer.parseInt(st[i]);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = obj.findUnion(arr1, arr2, N, M);
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println(); 
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//arr1,arr2 : the arrays
+// n, m: size of arrays
+
+
+class Solution {
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m) {
+        HashSet<Integer> ss = new HashSet<>();
+        
+        for (int num : arr1) {
+            ss.add(num);
+        }
+        
+        HashSet<Integer> sm = new HashSet<>();
+        for (int num : arr2) {
+            sm.add(num);
+        }
+        
+        ss.addAll(sm);
+        
+        ArrayList<Integer> result = new ArrayList<>(ss);
+        Collections.sort(result);
+        
+        return result;
+    }
+}
+
+
+
+
