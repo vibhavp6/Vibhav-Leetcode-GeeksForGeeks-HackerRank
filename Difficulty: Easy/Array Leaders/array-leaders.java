18240800f3b9f0b6dc7098f4fd33b4ede25c743d
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-09-14 14:02:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Array {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter ot = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+
+        while (t-- > 0) {
+
+            // input size of array
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+
+            // inserting elements in the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Solution obj = new Solution();
+
+            StringBuffer str = new StringBuffer();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+
+            // calling leaders() function
+            res = obj.leaders(n, arr);
+
+            for (int i = 0; i < res.size(); i++) {
+                ot.print(res.get(i) + " ");
+            }
+
+            ot.println();
+        }
+        ot.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int n, int arr[]) {
+        // Your code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        int max = arr[arr.length-1];
+        ans.add(max);
+        for (int i = arr.length-2; i>=0; i-- ) {
+            if (arr[i] >= max)  {
+                ans.add(arr[i]);
+                max = arr[i];
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
