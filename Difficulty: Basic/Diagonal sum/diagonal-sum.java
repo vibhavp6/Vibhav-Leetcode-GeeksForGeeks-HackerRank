e7285dc5ed394ad175784ea9f79e26ec8f6ce331
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-09-26 18:52:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Diagonal sum/diagonal-sum.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] matrix = new int[n][n];
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < n; j++)
+                    matrix[i][j] = Integer.parseInt(S[j]);
+            }
+            Solution ob = new Solution();
+            int ans = ob.DiagonalSum(matrix);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    public int DiagonalSum(int[][] matrix)
+    {
+        // code here
+        int n = matrix.length;
+        int i = 0; 
+        int j = n-1;
+        int sum = 0;
+        while(i<n && j>=0) {
+            sum += matrix[i][i];
+            sum += matrix[i][j];
+            i++;
+            j--;
+            
+        }
+        return sum;
+    }
+}
