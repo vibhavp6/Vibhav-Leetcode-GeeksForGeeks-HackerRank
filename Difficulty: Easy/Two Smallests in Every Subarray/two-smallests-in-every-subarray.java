1a6a186fb402bf63d793d5e33aaae882d5511477
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-10-12 12:57:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Smallests in Every Subarray/two-smallests-in-every-subarray.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int[] a = new int[s.length];
+            for (int i = 0; i < s.length; i++) a[i] = Integer.parseInt(s[i]);
+
+            Solution obj = new Solution();
+            int res = obj.pairWithMaxSum(a);
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public int pairWithMaxSum(int[] arr) {
+        // code here
+         if(arr.length<2)
+        {
+            return -1;
+        }
+        int sum=0;
+        for(int i=0;i<arr.length-1;i++)
+        {
+            sum=Math.max(sum, arr[i]+arr[i+1]);
+        }
+        return sum;
+    }
+}
