dbcf9b5f6452709fe011853a7aa9146e3ff4e1c4
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-10-13 14:12:08 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prime Pair with Target Sum/prime-pair-with-target-sum.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.ArrayList;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.getPrimes(n);
+
+            IntArray.print(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<Integer> getPrimes(int n) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 2; i <= n / 2; i++) {
+            if (isPrime(i) && isPrime(n - i)) {
+                result.add(i);
+                result.add(n - i);
+                return result;
+            }
+        }
+        result.add(-1);
+        result.add(-1);
+        return result;
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
