c749e1061f998957866bf7ef3fdd848f8cddb3ab
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-10-13 13:27:42 +0530

    Time: 2143 ms (5.01%) | Memory: 40.3 MB (42.93%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
         int sum=0;
-        for(int i=1;i<num;i++){
+        for(int i=1;i<=num/2;i++)
+        {
             if(num%i==0){
-              sum+=i;
+                sum=sum+i;
             }
         }
-        if(sum==num){
-            return true;
-        }
-        return false;
+        return sum==num;
+        
     }
 }
