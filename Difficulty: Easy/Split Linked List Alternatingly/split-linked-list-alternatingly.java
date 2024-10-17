b26ce2f47da1a8b0652367a6e07afa3a105dac82
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-10-17 18:56:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split Linked List Alternatingly/split-linked-list-alternatingly.java
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class Main {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = sc.nextLine();
+            Scanner ss = new Scanner(input);
+
+            while (ss.hasNextInt()) {
+                arr.add(ss.nextInt());
+            }
+
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+
+            Solution ob = new Solution();
+            Node[] result = ob.alternatingSplitList(head);
+            printList(result[0]);
+            printList(result[1]);
+        }
+
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+    // Function to append a new node with newData at the end of a linked list
+    Node[] alternatingSplitList(Node head) {
+        // code here
+        Node temp = head;
+        Node prev =null;
+        Node sec = head.next;
+        Node second =sec;
+        
+        
+        
+        while(temp !=null && temp.next !=null && sec !=null && sec.next!=null){
+            temp.next=temp.next.next;
+            sec.next=temp.next.next;
+            temp =temp.next;
+            sec =sec.next;
+           
+        }
+        
+        if(temp!=null && temp.next!=null){
+            temp.next =null;
+        }
+        if( sec!=null && sec.next!=null){
+            sec.next =null;
+        }
+                              
+        return new Node[]{head,second};
+
+    }
+}
