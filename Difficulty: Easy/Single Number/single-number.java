4d30c0a0f701f1a30ea37a113f22bd209fd43a69
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-10-18 11:58:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Single Number/single-number.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    int getSingle(int arr[]) {
+        // code here
+         int n=arr.length;
+        int temp=arr[0];
+        for(int i=1;i<n;i++){
+            temp^=arr[i];
+        }
+        return temp;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            // int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.getSingle(arr);
+
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
