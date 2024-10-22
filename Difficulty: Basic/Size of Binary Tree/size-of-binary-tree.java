5a2ac9b1a7e34627e051b7d92d03660514c8a237
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-10-22 19:46:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Size of Binary Tree/size-of-binary-tree.java
@@ -0,0 +1,145 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+
+    public static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length()==0 || str.charAt(0)=='N')
+            return null;
+
+        // Creating array of Strings from input
+        // String after spliting by space
+        String ip[] = str.split(" ");
+
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+
+        // Push the root to the queue
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        // Starting from the second element
+        int i = 1;
+        while(queue.size()>0 && i < ip.length)
+        {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if(!currVal.equals("N"))
+            {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if(!currVal.equals("N"))
+            {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static Node inputTree(BufferedReader br) throws IOException
+    {
+        return buildTree(br.readLine().trim());
+    }
+
+    public static void inorder(Node root)
+    {
+        if (root == null)
+           return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            Node node = Node.inputTree(br);
+            
+            Solution obj = new Solution();
+            int res = obj.getSize(node);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for Binary Tree Node
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    public static int getSize(Node node) {
+        // code here
+        // code here
+        if(node ==null){
+            return 0;
+        }
+        return (1 + getSize(node.left) + getSize(node.right));
+    }
+}
+        
