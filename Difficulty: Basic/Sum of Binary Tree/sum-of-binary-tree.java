f194a4fb251615d401ad8d743273a4283d17162d
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-10-22 19:59:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+class GfG{
+    
+    public static void main(String[] args)throws IOException{
+        //Scanner sc=new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t=Integer.parseInt(br.readLine());
+       
+        while(t > 0){
+            Queue<Node> q = new LinkedList<>();
+        
+            int n = Integer.parseInt(br.readLine());
+            String input[] = br.readLine().trim().split(" ");
+            
+            Node root = null;
+            int j=0;
+            while(n > 0){
+            int a1 = Integer.parseInt(input[j]);
+            int a2 = Integer.parseInt(input[j+1]);
+            char lr = input[j+2].charAt(0);
+            j+=3;
+            
+            if(root == null)
+            {
+                root = new Node(a1);
+                q.add(root);
+            }
+            
+            Node pick = q.peek();
+            
+            q.remove();
+            
+            if(lr=='L'){
+                pick.left = new Node(a2);
+                q.add(pick.left);
+            }
+            a1 = Integer.parseInt(input[j]);
+            a2 = Integer.parseInt(input[j+1]);
+            lr = input[j+2].charAt(0);
+            j += 3;
+            
+            if(lr=='R'){
+                pick.right = new Node(a2);
+                q.add(pick.right);
+            }
+            
+            n-=2;
+        }
+        System.out.println(BinaryTree.sumBT(root));
+            t--;
+            
+        }
+    }
+   
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class BinaryTree
+{
+    static int sumBT(Node head){
+        //Code
+        if (head == null) return 0;
+        return sumBT(head.left) + sumBT(head.right) + head.data;
+        
+    }
+    
+}
