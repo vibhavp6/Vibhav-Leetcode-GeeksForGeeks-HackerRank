71912cbb72d9ccc15f1cba9e3572cd6101289f1a
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-10-26 13:26:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (27.24%) - LeetSync

---
+++ 112-path-sum/path-sum.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+
+        // Check if the node is a leaf node
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+
+        // Recursive calls for left and right subtrees
+        boolean leftans = hasPathSum(root.left, targetSum - root.val);
+        boolean rightans = hasPathSum(root.right, targetSum - root.val);
+
+        // Return true if either leftans or rightans is true
+        if (leftans) {
+            return true;
+        } else if (rightans) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
