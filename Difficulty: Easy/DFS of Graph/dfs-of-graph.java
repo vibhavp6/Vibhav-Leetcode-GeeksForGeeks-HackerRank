99518ad4fd240ead63ee2a50a2418ff555f52925
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-11-11 22:42:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(adj);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+          int V = adj.size(); // Get the number of vertices from the size of adj
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean[] vis = new boolean[V];
+        
+        dfs (0 , adj , vis , ans);
+        
+        return ans;
+         
+    }
+    
+    public void dfs (int node, ArrayList<ArrayList<Integer>> adj,boolean[] vis, ArrayList<Integer> ans ) {
+        vis[node] = true;
+        ans.add(node);
+        
+        for (Integer it : adj.get(node)) {
+            if (!vis[it]) {
+                dfs(it,adj,vis,ans);
+            }
+        }
+    }
+}
