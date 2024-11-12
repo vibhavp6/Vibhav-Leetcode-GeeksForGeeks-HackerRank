be728686413ec0707efe9f69a640090710ecfdfb
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-11-12 20:00:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Meeting Rooms/meeting-rooms.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] arr = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                String temp[] = br.readLine().trim().split(" ");
+                arr[i][0] = Integer.parseInt(temp[0]);
+                String x = temp[1];
+                arr[i][1] = Integer.parseInt(x);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.canAttend(arr);
+            if (ans)
+                System.out.println("true");
+            else
+                System.out.println("false");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static boolean canAttend(int[][] arr) {
+        // Your code here
+        Arrays.sort(arr,(a,b)->a[1]-b[1]);
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i][1]>arr[i+1][0]) return false;
+        }
+        return true;
+    }
+}
