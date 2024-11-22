6b1aa7816e0af18ca6159b285a16c6d3136d2f2b
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-11-22 22:53:22 +0530

    Time: 24 ms (92.13%) | Memory: 58.2 MB (75.37%) - LeetSync

---
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxEqualRowsAfterFlips(int[][] mat) {
+        Map<String, Integer> patFreq = new HashMap<>();
+        
+        for (int[] row : mat) {
+            StringBuilder pattern = new StringBuilder();
+            if (row[0] == 0) {
+                for (int bit : row) pattern.append(bit);
+            } else {
+                for (int bit : row) pattern.append(bit ^ 1);
+            }
+            patFreq.merge(pattern.toString(), 1, Integer::sum);
+        }
+        
+        return Collections.max(patFreq.values());
+    }
+}
