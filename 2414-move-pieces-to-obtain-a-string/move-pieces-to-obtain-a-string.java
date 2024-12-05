c098ab175f97636616724a8280d87617ea36a37c
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-12-05 11:06:48 +0530

    Time: 12 ms (92.35%) | Memory: 45.5 MB (54.17%) - LeetSync

---
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean canChange(String start, String target) {
+        if (start.equals(target)) {
+            return true;
+        }
+        int waitL = 0;
+        int waitR = 0;
+        
+        for (int i = 0; i < start.length(); i++) {
+            char curr = start.charAt(i);
+            char goal = target.charAt(i);
+            
+            if (curr == 'R') {
+                if (waitL > 0) {
+                    return false;
+                }
+                waitR++;
+            }
+            if (goal == 'L') {
+                if (waitR > 0) {
+                    return false;
+                }
+                waitL++;
+            }
+            if (goal == 'R') {
+                if (waitR == 0) {
+                    return false;
+                }
+                waitR--;
+            }
+            if (curr == 'L') {
+                if (waitL == 0) {
+                    return false;
+                }
+                waitL--;
+            }
+        }
+        
+        return waitL == 0 && waitR == 0;
+    }
+}
