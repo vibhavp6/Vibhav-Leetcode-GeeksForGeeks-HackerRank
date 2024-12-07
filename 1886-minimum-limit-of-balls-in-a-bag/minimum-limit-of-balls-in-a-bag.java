bcc3b16cfa544a03d8588d7dfcc6ec8dd9eb5fb1
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-12-07 10:49:02 +0530

    Time: 29 ms (75.95%) | Memory: 59.8 MB (90.28%) - LeetSync

---
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumSize(int[] nums, int maxOps) {
+        int low = 1, high = Arrays.stream(nums).max().getAsInt();
+        while (low < high) {
+            int mid = (low + high) / 2;
+            int ops = 0;
+            for (int n : nums) {
+                ops += (n - 1) / mid;
+            }
+            if (ops <= maxOps) high = mid;
+            else low = mid + 1;
+        }
+        return high;
+    }
+}
