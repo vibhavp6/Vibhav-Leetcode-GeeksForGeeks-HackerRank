dda767e32d45f751200ccb9ce614b4b2e79e2b6e
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-12-09 23:05:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Insert Interval/insert-interval.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[][] = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                a[i][0] = sc.nextInt();
+                a[i][1] = sc.nextInt();
+            }
+            int h[] = new int[2];
+            h[0] = sc.nextInt();
+            h[1] = sc.nextInt();
+            Solution obj = new Solution();
+            ArrayList<int[]> ans = obj.insertInterval(a, h);
+            System.out.print("[");
+            for (int i = 0; i < ans.size(); i++) {
+                System.out.print("[");
+                System.out.print(ans.get(i)[0] + "," + ans.get(i)[1]);
+                System.out.print("]");
+                if (i != ans.size() - 1) System.out.print(",");
+            }
+            System.out.println("]");
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static ArrayList<int[]> insertInterval(int[][] intervals, int[] newInterval) {
+        // code here
+        ArrayList<int[]> al=new ArrayList<>();
+        boolean added=false;
+        for(int i=0;i<intervals.length;i++)
+        {
+            if(intervals[i][0]<newInterval[0]&&intervals[i][1]<newInterval[0])
+            {
+                al.add(intervals[i]);
+            }
+            else if(intervals[i][0]>newInterval[1]&&intervals[i][1]>newInterval[1])
+            {
+                if(added==false)
+                {
+                    al.add(newInterval);
+                    added=true;
+                }
+                al.add(intervals[i]);
+            }
+            else
+            {
+                newInterval[0]=Math.min(newInterval[0],intervals[i][0]);
+                newInterval[1]=Math.max(newInterval[1],intervals[i][1]);
+            }
+        }
+        if(added==false)
+        {
+            al.add(newInterval);
+        }
+        return al;
+    }
+}
