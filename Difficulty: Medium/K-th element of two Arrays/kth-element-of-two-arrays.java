61d7af9d1c6f5ea839ad1b321964f66a0ac43055
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-12-16 22:13:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine().trim());
+
+            String[] line1 = br.readLine().trim().split(" ");
+            int[] a = new int[line1.length];
+            for (int i = 0; i < line1.length; i++) {
+                a[i] = Integer.parseInt(line1[i]);
+            }
+
+            String[] line2 = br.readLine().trim().split(" ");
+            int[] b = new int[line2.length];
+            for (int i = 0; i < line2.length; i++) {
+                b[i] = Integer.parseInt(line2[i]);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.kthElement(a, b, k));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int kthElement(int a[], int b[], int k) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        
+        for(int i :b){
+            arr.add(i);
+        }
+        for(int i :a){
+            arr.add(i);
+        }
+        Collections.sort(arr);
+        
+        return arr.get(k-1);
+    }
+}
