ced3169453276d0cd51099cd1bee07c0345e81d3
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2024-12-29 12:08:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of Two arrays with Duplicate Elements/intersection-of-two-arrays-with-duplicate-elements.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            // First array input
+            String[] str1 = br.readLine().trim().split(
+                " "); // Read the first line and split by spaces
+            int n = str1.length;
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str1[i]); // Convert each element to an integer
+            }
+
+            // Second array input
+            String[] str2 = br.readLine().trim().split(
+                " "); // Read the second line and split by spaces
+            int m = str2.length;
+            int[] b = new int[m];
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(str2[i]); // Convert each element to an integer
+            }
+
+            Solution sol = new Solution();
+            ArrayList<Integer> res = sol.intersectionWithDuplicates(a, b);
+
+            // Sort the result
+            Collections.sort(res);
+
+            // Print the result
+            if (res.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int num : res) {
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public ArrayList<Integer> intersectionWithDuplicates(int[] a, int[] b) {
+        // code here
+        ArrayList<Integer> List = new ArrayList<>();
+        HashSet<Integer> aList = new HashSet<>();
+        HashSet<Integer> bList = new HashSet<>();
+        
+        for(int num : a){
+            aList.add(num);
+        }
+        for(int num : b){
+            bList.add(num);
+        }
+        for(int val : aList){
+            if(bList.contains(val)){
+                List.add(val);
+            }
+        }
+        return List;
+    }
+}
