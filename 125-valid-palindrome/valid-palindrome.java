21fcca563bc6f3b91d0c45367fa920def215abf8
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-01-01 15:51:13 +0530

    Time: 2 ms (99.04%) | Memory: 43 MB (76.21%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        //check string is empty or not
         if (s.isEmpty()) {
             return true;
         }
-       // setting the pointer to first and last index of the string
 
         int left = 0;
         int right = s.length() - 1;
@@ -12,14 +10,12 @@ class Solution {
         while (left < right) {
             char leftChar = s.charAt(left);
             char rightChar = s.charAt(right);
-            //if the character at left side is not equal to digit or letter then skip it and move on 
             if (!Character.isLetterOrDigit(leftChar)) {
                 left++;
-             //if the character at right side is not equal to digit or letter then skip it and move on 
             } else if (!Character.isLetterOrDigit(rightChar)) {
                 right--;
             } else {
-                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                if (Character.toUpperCase(leftChar) != Character.toUpperCase(rightChar)) {
                     return false;
                 }
                 left++;
