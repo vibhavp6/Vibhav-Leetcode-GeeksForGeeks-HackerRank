0c1afc4eedbe01cf41c4826e4b78f44c8b013e62
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-01-06 14:26:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum Pair closest to target/sum-pair-closest-to-target.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            ArrayList<Integer> array = new ArrayList<>();
+
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int target = Integer.parseInt(br.readLine());
+
+            Solution ob = new Solution();
+            List<Integer> res = ob.sumClosest(arr, target);
+            if (res.isEmpty()) {
+                System.out.print("[]");
+            } else {
+                for (Integer num : res) {
+                    System.out.print(num + " ");
+                }
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public List<Integer> sumClosest(int[] arr, int target) {
+        int n=arr.length;
+        Arrays.sort(arr);
+        int left=0;
+        int  right=n-1;
+        int minDiff =  Integer.MAX_VALUE;
+        List<Integer>Li=new ArrayList<>();
+
+        
+        while(left<right){
+            int sum=arr[left]+arr[right];
+            int diff = Math.abs(target-sum);
+            
+            if(diff<minDiff){
+               minDiff=diff;
+               Li=Arrays.asList(arr[left],arr[right]);
+                
+            }if (sum>target){
+                right--;
+            }else if(sum<target){
+                left++;
+            }else{
+                return Arrays.asList(arr[left],arr[right]);
+            }
+        }
+        return Li;
+        // code here
+    }
+}
