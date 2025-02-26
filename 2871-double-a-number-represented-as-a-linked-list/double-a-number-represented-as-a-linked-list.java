a22139ed816ea061237e007be3eb3220b9cd082b
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-02-26 12:31:58 +0530

    Time: 4 ms (80.32%) | Memory: 47 MB (40.05%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -9,35 +9,40 @@
  * }
  */
 class Solution {
-    public ListNode reverse (ListNode head) {
-        ListNode curr = head;
+    public ListNode revLL(ListNode head) {
+        ListNode temp = head;
         ListNode prev = null;
-        while (curr!= null) {
-            ListNode nextnode = curr.next;
-            curr.next =prev;
-            prev = curr;
-            curr = nextnode;
-        }
-        return prev;
-     }
-    public ListNode doubleIt(ListNode head) {
-        ListNode ans =  new ListNode(-1);
-        ListNode var = ans;
-        ListNode tempa = reverse (head);
 
+        while (temp != null) {
+            ListNode nextNode = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = nextNode;
+        }
+
+        return prev;
+    }
+
+    public ListNode doubleIt(ListNode head) {
+        ListNode rvs = revLL(head);
+        ListNode curr = rvs;
         int carry = 0;
 
-        while (tempa!= null ||  carry != 0) {
-            int sum = carry;
-            if (tempa!= null) {
-                sum += (tempa.val)*2;
-                tempa =tempa.next;
-            }
-            carry = sum/10;
-            var.next = new ListNode(sum%10);
-            var = var.next;
+        while (curr != null) {
+            int sum = carry + (curr.val * 2);
+            curr.val = sum % 10;
+            carry = sum / 10;
+            curr = curr.next;
         }
-        return reverse(ans.next);
 
+        head = revLL(rvs);
+
+        if (carry > 0) {
+            ListNode newNode = new ListNode(carry);
+            newNode.next = head;
+            return newNode;
+        }
+
+        return head;
     }
 }
