8d39ce13057ecbe900c7832668a3df09b0217a58
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-02-26 12:12:49 +0530

    Time: 4 ms (80.32%) | Memory: 45.8 MB (88.05%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -8,58 +8,36 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-public class Solution {
+class Solution {
+    public ListNode reverse (ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr!= null) {
+            ListNode nextnode = curr.next;
+            curr.next =prev;
+            prev = curr;
+            curr = nextnode;
+        }
+        return prev;
+     }
     public ListNode doubleIt(ListNode head) {
-        // Reverse the linked list
-        ListNode reversedList = reverseList(head);
-        // Initialize variables to track carry and previous node
+        ListNode ans =  new ListNode(-1);
+        ListNode var = ans;
+        ListNode tempa = reverse (head);
+
         int carry = 0;
-        ListNode current = reversedList, previous = null;
 
-        // Traverse the reversed linked list
-        while (current != null) {
-            // Calculate the new value for the current node
-            int newValue = current.val * 2 + carry;
-            // Update the current node's value
-            current.val = newValue % 10;
-            // Update carry for the next iteration
-            if (newValue > 9) {
-                carry = 1;
-            } else {
-                carry = 0;
+        while (tempa!= null ||  carry != 0) {
+            int sum = carry;
+            if (tempa!= null) {
+                sum += (tempa.val)*2;
+                tempa =tempa.next;
             }
-            // Move to the next node
-            previous = current;
-            current = current.next;
+            carry = sum/10;
+            var.next = new ListNode(sum%10);
+            var = var.next;
         }
+        return reverse(ans.next);
 
-        // If there's a carry after the loop, add an extra node
-        if (carry != 0) {
-            ListNode extraNode = new ListNode(carry);
-            previous.next = extraNode;
-        }
-
-        // Reverse the list again to get the original order
-        ListNode result = reverseList(reversedList);
-
-        return result;
-    }
-
-    // Method to reverse the linked list
-    public ListNode reverseList(ListNode node) {
-        ListNode previous = null, current = node, nextNode;
-
-        // Traverse the original linked list
-        while (current != null) {
-            // Store the next node
-            nextNode = current.next;
-            // Reverse the link
-            current.next = previous;
-            // Move to the next nodes
-            previous = current;
-            current = nextNode;
-        }
-        // Previous becomes the new head of the reversed list
-        return previous;
     }
 }
