eb622f2ed2f8f3dd7029d7ba6ec5f2d99ceaa1db
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-03-05 00:12:52 +0530

    Time: 43 ms (59.77%) | Memory: 56.1 MB (33.85%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -1,40 +1,36 @@
 class Solution {
-    public int maxDistance(int[] position, int m) {
-        Arrays.sort(position);
-        int lo = 1;
-        int hi = (position[position.length-1]-position[0])/(m-1);
-        int ans = 1;
-        while(lo<=hi)
-        {
-            int mid = lo+(hi-lo)/2;
-            if(canWePlace(position, mid, m))
-            {
-                ans = mid;
-                lo=mid+1;
+   public static int check (int mid , int [] stalls , int k) {
+        int n = stalls.length;
+        int count = 1;
+        int prev =stalls[0];
+        for (int i = 0; i<n; i++) {
+            if(stalls[i] - prev >= mid) {
+                count++;
+                prev = stalls[i];
             }
-            else
-            {
-                hi = mid-1;
+            if (count>=k) return 1;
+        }
+        return 0;
+        
+    }
+    public static int maxDistance(int[] stalls, int k) {
+        // code here
+        Arrays.sort(stalls);
+        int n = stalls.length;
+        int left = 1, right = stalls[n-1] - stalls[0] , ans =-1;
+        while (left<=right) {
+            int mid = left + (right-left)/2;
+            int flag = check(mid,stalls,k);
+            if (flag == 1) {
+                ans = mid;
+                left = mid +1;
+            }
+            else {
+                right = mid -1;
             }
         }
         return ans;
     }
-    private boolean canWePlace(int[] arr, int dist, int cows)
-    {
-        int cntCows=1;
-        int last = arr[0];
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]-last>=dist)
-            {
-                cntCows++;
-                last = arr[i];
-            }
-            if(cntCows>=cows)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
+
 }
+ 
