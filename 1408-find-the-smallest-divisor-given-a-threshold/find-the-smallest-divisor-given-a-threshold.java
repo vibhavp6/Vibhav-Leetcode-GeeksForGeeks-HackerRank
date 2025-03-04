f67aadbcd87bacd5e87dc36d0426653e786f1da9
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-03-04 17:54:12 +0530

    Time: 10 ms (79.49%) | Memory: 48.2 MB (65.04%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -0,0 +1,37 @@
+
+class Solution {
+    public int check (int mid , int[] piles , int h) {
+        int ans = 0;
+        for (int i = 0; i <piles.length; i++) {
+            ans +=piles[i]/mid;
+            if(piles[i]%mid != 0) {
+                ans += 1;
+            }
+            if (ans>h) return 1;
+        } 
+        return 0;
+    } 
+    public int smallestDivisor(int[] piles, int h) {
+
+        int left = 1 , right = -1 , ans = -1;
+        int maxvalue = piles[0];
+        for (int i = 0; i<piles.length; i++) {
+            if(piles[i] > maxvalue){
+                maxvalue = piles[i];
+            }
+            right = maxvalue;
+        }
+        while(left<=right) {
+            int mid = left +(right-left)/2;
+            int flg = check(mid,piles,h);
+            if(flg == 1) {
+                left = mid+1;
+            }
+            else if(flg == 0) {
+                ans = mid;
+                right = mid-1;
+            }
+        }
+        return ans;
+    }
+}
