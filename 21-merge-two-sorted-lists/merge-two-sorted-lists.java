038983ab8c4444b71246432e4918f8f206b814fc
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-03-06 23:41:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (33.07%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -13,29 +13,28 @@ class Solution {
         ListNode dummy = new ListNode(-1);
         ListNode pt = dummy;
         ListNode l1 = list1;
-        ListNode l2 = list2;
-        while (l1 != null || l2 != null) {
-            if((l1!= null && l2!= null) && (l1.val <= l2.val )) {
-                pt.next = l1;
+        ListNode  l2 = list2;
+        while(l1!= null || l2!= null){
+            if (l1 != null && l2 != null && (l1.val <= l2.val)) {
+                pt.next= l1;
                 pt = pt.next;
                 l1 = l1.next;
             }
-            else if((l1!= null && l2!= null) && (l2.val < l1.val ) ) {
-                pt.next = l2;
+            else if (l1 != null && l2 != null && (l1.val > l2.val)) {
+                pt.next= l2;
                 pt = pt.next;
                 l2 = l2.next;
             }
             else if (l1 != null) {
-                pt.next = l1;
+                pt.next= l1;
                 pt = pt.next;
                 l1 = l1.next;
             }
-            else if (l2 != null) {
-                pt.next = l2;
+            else if (l2 != null){
+                pt.next= l2;
                 pt = pt.next;
                 l2 = l2.next;
             }
-
         }
         return dummy.next;
     }
