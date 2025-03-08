2ae2546384584dec668fbc8f942c4b217b6836fc
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-03-08 19:07:14 +0530

    Time: 56 ms (23.34%) | Memory: 58.9 MB (37.53%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,25 +1,21 @@
 class Solution {
-    public long maximumSubarraySum(int[] nums, int k) {
-        Map<Integer, Integer> mpp = new HashMap<>();
-        long sum = 0, ans = 0;
+    public long maximumSubarraySum(int[] arr, int k) {
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        long sum = 0;
+        long ans = 0;
         int st = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            mpp.put(nums[i], mpp.getOrDefault(nums[i], 0) + 1);
-            sum += nums[i];
-
-            if (i - st + 1 == k) {
-                if (mpp.size() == k) {
-                    ans = Math.max(ans, sum);
+        for(int i =0; i <arr.length; i++){
+            mpp.put(arr[i],mpp.getOrDefault(arr[i] , 0) + 1);
+            sum += arr[i];
+            if(i-st+1 == k) {
+                if(mpp.size() == k) {
+                 ans = Math.max (ans,sum);
                 }
-
-                mpp.put(nums[st], mpp.get(nums[st]) - 1);
-                sum -= nums[st];
-
-                if (mpp.get(nums[st]) == 0) {
-                    mpp.remove(nums[st]);
+               mpp.put(arr[st] ,mpp.get(arr[st]) -1);
+                sum -= arr[st];
+                if(mpp.get(arr[st]) == 0){
+                  mpp.remove(arr[st]);
                 }
-
                 st++;
             }
         }
