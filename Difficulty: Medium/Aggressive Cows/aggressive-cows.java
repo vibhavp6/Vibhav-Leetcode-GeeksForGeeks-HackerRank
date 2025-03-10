67fdb702589a01721e80d59f7bacea4f47165bd6
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-03-10 13:03:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -31,37 +31,36 @@ class GFG {
 
 // User function Template for Java
 class Solution {
-    public static int check (long mid , int []  stalls , int k) {
-        int n = stalls.length;
+    public static int check(long mid, int [] stalls , int k){
         int count = 1;
         int prev = stalls[0];
-        for (int i = 0; i<n; i++) {
-            if (stalls[i] - prev >= mid){
+        for(int i =0; i<stalls.length;i++){
+            if(stalls[i] - prev >= mid) {
                 count++;
                 prev = stalls[i];
             }
-            if (count >= k) return 1;
+            if(count >= k) return 1;
         }
         return 0;
     }
     public static int aggressiveCows(int[] stalls, int k) {
-        Arrays.sort(stalls);
+        
         // code here
+        Arrays.sort(stalls);
         int n = stalls.length;
-        long left = 1 , right = stalls[n-1],ans = -1;
-        while (left<=right){
+        long left = 1 ,  right =stalls[n-1] - stalls[0], ans = -2;
+        while(left<=right) {
             long mid = left + (right-left)/2;
-            long flag  =  check(mid , stalls , k);
-            if (flag == 1) {
-                ans = mid ;
-                left = mid +1;
+            int flg = check(mid,stalls,k);
+            if(flg == 1){
+                ans = mid;
+                left = mid+1;
             }
-            else {
-               if (flag == 0)  {
-                   right = mid -1;
-               }
+            else if (flg == 0){
+                right = mid-1;
+                
             }
         }
-        return (int)ans ;
+        return(int) ans;
     }
 }
