4925d97d60566c0431694959ca0c1a568fb6ebe8
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-03-10 11:27:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.java
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -9,12 +9,13 @@ class GFG {
         int T = Integer.parseInt(br.readLine().trim());
         while (T-- > 0) {
             String s1 = br.readLine();
-            String[] S = s1.split(" ");
-            String s = S[0];
-            String t = S[1];
+            String s2 = br.readLine();
+
             Solution ob = new Solution();
-            int ans = ob.editDistance(s, t);
+            int ans = ob.editDistance(s1, s2);
             System.out.println(ans);
+
+            System.out.println("~");
         }
     }
 }
@@ -23,26 +24,26 @@ class GFG {
 
 
 class Solution {
-    public int editDistance(String str1, String str2) {
-        int n=str1.length();
-        int m=str2.length();
-        int dp[][]=new int[n+1][m+1];
-        
-        for(int j=0;j<=m;j++) dp[0][j]=j;
-        for(int i=0;i<=n;i++)dp[i][0]=i;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(str1.charAt(i-1)==str2.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1];
-                }else {
-                    int insert=1+dp[i][j-1];
-                    int remove=1+dp[i-1][j];
-                    int replace=1+dp[i-1][j-1];
-                    
-                    dp[i][j]=Math.min(insert, Math.min(remove,replace));
+    public int editDistance(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j],  
+                                    Math.min(dp[i][j - 1],  
+                                             dp[i - 1][j - 1])); 
                 }
-            }   
+            }
         }
-        return dp[n][m];
+        return dp[m][n];
     }
 }
