71d0460a2a625d8835bc230d86120a0f29bd95a9
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-03-10 12:22:32 +0530

    Time: 31 ms (25.95%) | Memory: 45.1 MB (34.63%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,17 +1,18 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int st = 0 , maxfreq = 0, ans = 0;
-        HashMap<Character ,Integer> mpp = new HashMap  <>();
-        for(int i =0; i<s.length() ; i++) {
-            mpp.put(s.charAt(i) , mpp.getOrDefault(s.charAt(i) , 0) +1);
-            maxfreq = Math.max(maxfreq,mpp.get(s.charAt(i)));
-
-            while((i-st+1 )- maxfreq > k) {
-                mpp.put(s.charAt(st) , mpp.get(s.charAt(st)) -1 );
+        int st = 0;
+        int ans = 0;
+        int maxfreq = 0;
+        HashMap<Character , Integer> mpp = new HashMap<>();
+        for(int i =0;i<s.length();i++){
+            mpp.put(s.charAt(i) ,mpp.getOrDefault(s.charAt(i) , 0) +1);
+            maxfreq =  Math.max(maxfreq , mpp.get(s.charAt(i)));
+            while((i - st +1 ) - maxfreq > k) {
+                mpp.put(s.charAt(st) , mpp.get(s.charAt(st)) -1);
                 if(mpp.get(s.charAt(st)) == 0) mpp.remove(s.charAt(st));
                 st++;
             }
-            ans = Math.max(i-st+1,ans);
+            ans = Math.max(ans, i -st+1);
         }
         return ans;
     }
