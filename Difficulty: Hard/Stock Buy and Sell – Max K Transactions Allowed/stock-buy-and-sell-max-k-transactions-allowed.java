3b4a324488bce660b837e57a0ca924b4659aa15d
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-03-19 19:12:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Stock Buy and Sell – Max K Transactions Allowed/stock-buy-and-sell-max-k-transactions-allowed.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < tc; t++) {
+            String[] input = br.readLine().split(" ");
+            int arr[] = new int[input.length];
+
+            for (int i = 0; i < arr.length; i++) arr[i] = Integer.parseInt(input[i]);
+
+            // Read the integer k
+            int k = Integer.parseInt(br.readLine());
+
+            // Call the solution function
+            Solution obj = new Solution();
+            System.out.println(obj.maxProfit(arr, k));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static int maxProfit(int prices[], int k) {
+        if (prices == null || prices.length == 0 || k == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+
+        
+        if (k >= n / 2) {
+            int maxProfit = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i - 1]) {
+                    maxProfit += prices[i] - prices[i - 1];
+                }
+            }
+            return maxProfit;
+        }
+
+
+        int[][] dp = new int[k + 1][n];
+
+        for (int i = 1; i <= k; i++) {
+            int maxDiff = -prices[0]; 
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(dp[i][j - 1], prices[j] + maxDiff);
+                maxDiff = Math.max(maxDiff, dp[i - 1][j] - prices[j]);
+            }
+        }
+
+        return dp[k][n - 1];
+    }
+}
+
