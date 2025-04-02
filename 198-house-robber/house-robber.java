ba2c049ffa93ac8457a5b100977b92beff1c072a
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-02 18:57:35 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (88.30%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,22 +1,23 @@
+import java.util.Arrays;
+
 class Solution {
-    private int solve(int index, int[] nums, int[] dp) {
-        if (index >= nums.length) return 0;
 
-        if (dp[index] != -1) return dp[index];
+    public int helper (int [] nums, int index , int []dp) {
+        if (index>= nums.length) return 0;
 
+        if (dp[index] != -1) return dp [index];
 
-        int take = nums[index] + solve(index + 2, nums, dp);
-        int notTake = solve(index + 1, nums, dp);
+        int take = nums[index] + helper(nums, index+2 , dp);
+        int nottake = helper(nums, index+1 ,dp);
 
-        return dp[index] = Math.max(take, notTake);
+        return dp[index] = Math.max(take , nottake);
     }
+
 
     public int rob(int[] nums) {
         int n = nums.length;
-
-        int[] dp = new int[n];
+        int [] dp = new int [n];
         Arrays.fill(dp, -1);
-
-        return solve(0, nums, dp);
+        return helper(nums, 0 , dp);
     }
 }
