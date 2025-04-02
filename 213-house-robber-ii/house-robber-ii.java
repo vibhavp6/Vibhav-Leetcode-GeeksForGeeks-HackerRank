c10b6ad6bcdf6981a2679fb1426636d25de1e410
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-02 19:04:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (27.11%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,27 +1,30 @@
+import java.util.Arrays;
+
 class Solution {
-    private int solve(int index, int[] nums, int[] dp) {
-        if (index >= nums.length) return 0;
-        if (dp[index] != -1) return dp[index];
 
-        int take = nums[index] + solve(index + 2, nums, dp);
-        int notTake = solve(index + 1, nums, dp);
+    public int helper (int [] nums, int index , int []dp) {
+        if (index>= nums.length) return 0;
 
-        return dp[index] = Math.max(take, notTake);
+        if (dp[index] != -1) return dp [index];
+
+        int take = nums[index] + helper(nums, index+2 , dp);
+        int nottake = helper(nums, index+1 ,dp);
+
+        return dp[index] = Math.max(take , nottake);
     }
+
 
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0];
-
+        if( n ==1) return nums[0];
         int[] nums1 = Arrays.copyOfRange(nums, 0, n - 1);
         int[] nums2 = Arrays.copyOfRange(nums, 1, n);
 
-        int[] dp1 = new int[n - 1];
-        int[] dp2 = new int[n - 1];
 
+        int [] dp1 = new int [n-1];
+        int [] dp2 = new int [n-1];
         Arrays.fill(dp1, -1);
         Arrays.fill(dp2, -1);
-
-        return Math.max(solve(0, nums1, dp1), solve(0, nums2, dp2));
+        return Math.max(helper(nums1 , 0 , dp1), helper(nums2, 0, dp2));
     }
 }
