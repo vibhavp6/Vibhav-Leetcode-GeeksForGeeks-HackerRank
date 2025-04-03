aad15d72f18025257b2237449bcfc0e0e0bcdc2c
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-03 20:48:23 +0530

    Time: 1 ms (99.52%) | Memory: 45.8 MB (19.44%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,33 +1,30 @@
 class Solution {
+    private int solve(int[][] matrix, int i, int j , int [][]dp) {
+        int n = matrix.length;
+        
+        if (i >= n) return 0;
+
+        if(dp[i][j] != Integer.MIN_VALUE) return dp[i][j];
+        int op1 = solve(matrix, i + 1, j,dp);
+        int op2 = Integer.MAX_VALUE;
+        int op3 = Integer.MAX_VALUE;
+
+        if (j - 1 >= 0) op2 = solve(matrix, i + 1, j - 1,dp);
+        if (j + 1 <= n - 1) op3 = solve(matrix, i + 1, j + 1,dp);
+
+        return dp[i][j] = matrix[i][j] + Math.min(Math.min(op1, op2), op3);
+    }
+
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
-        int m = matrix[0].length;
-        int[][] dp = new int[n][m];
-
-        for (int j = 0; j < m; j++) {
-            dp[0][j] = matrix[0][j];
+        int minSum = Integer.MAX_VALUE;
+        int [] [] dp = new int [n][n];
+        for (int [] row : dp) {
+            Arrays.fill(row , Integer.MIN_VALUE);
         }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int ld = Integer.MAX_VALUE, rd = Integer.MAX_VALUE;
-                int up = matrix[i][j] + dp[i - 1][j];
-
-                if (j - 1 >= 0) {
-                    ld = matrix[i][j] + dp[i - 1][j - 1];
-                }
-                if (j + 1 < m) {
-                    rd = matrix[i][j] + dp[i - 1][j + 1];
-                }
-
-                dp[i][j] = Math.min(up, Math.min(ld, rd));
-            }
+        for (int j = 0; j < n; j++) {
+            minSum = Math.min(minSum, solve(matrix, 0, j , dp));
         }
-
-        int mini = dp[n - 1][0];
-        for (int j = 1; j < m; j++) {
-            mini = Math.min(mini, dp[n - 1][j]);
-        }
-        return mini;
+        return minSum;
     }
 }
