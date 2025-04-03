a448234fbf15069875da0520d6ba95bad7b12d04
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-03 19:31:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (50.68%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Solution {
+    int[][] dp;
+
+    private int solve(int[][] grid , int n, int m, int i, int j) {
+        if (i >= n || j >= m) {
+            return 0;
+        }
+        if (grid [i][j] == 1) return 0;
+        
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (grid [i][j] == 1) return 0;
+
+        dp[i][j] = solve(grid , n, m, i + 1, j) + solve(grid , n, m, i, j + 1);
+        return dp[i][j];
+    }
+
+    public int uniquePathsWithObstacles(int [] [] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        dp = new int[n][m];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return solve(grid ,n, m, 0, 0);
+    }
+}
