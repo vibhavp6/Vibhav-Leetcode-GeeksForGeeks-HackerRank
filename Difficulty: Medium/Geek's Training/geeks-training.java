8975a0461982602b3fac2362b1af306fab62114f
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-04 13:48:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.java
+++ Difficulty: Medium/Geek's Training/geeks-training.java
@@ -29,48 +29,46 @@ class GFG {
 
 
 // User function Template for Java
+
 class Solution {
-    private int solve(int arr[][], int precol, int i, int[][]dp) {
+    public int solve(int [][] arr , int i , int precol ,  int [][] dp) {
         int n = arr.length;
         int m = arr[0].length;
-
-        if (i == n) return 0; 
-        
+        if(i >= n || precol>= m) return 0;
         if(dp[i][precol] != -1) return dp[i][precol];
-
         int op1 = Integer.MIN_VALUE;
         int op2 = Integer.MIN_VALUE;
+        if(precol == 0) {
+            op1 = arr[i][1]  + solve(arr , i+1 , 1 , dp);
+            op2 = arr[i][2] + solve(arr , i+1 , 2, dp);
+            
+        }
+        if(precol == 1) {
+            op1 = arr[i][0]  + solve(arr , i+1 , 0 , dp);
+            op2 = arr[i][2] + solve(arr , i+1 , 2, dp);
+            
+        }
+        if(precol == 2) {
+            op1 = arr[i][0]  + solve(arr , i+1 , 0 , dp);
+            op2 = arr[i][1] + solve(arr , i+1 , 1, dp);
+            
+        }
         
-        if(precol == 0){
-            op1 = arr[i][1] + solve(arr, 1, i + 1,dp);
-            op2 = arr[i][2] + solve(arr, 2, i + 1,dp);
-        }
-        if(precol == 1){
-            op1 = arr[i][0] + solve(arr, 0, i + 1,dp);
-            op2 = arr[i][2] + solve(arr, 2, i + 1,dp);
-        }
-        if(precol == 2){
-            op1 = arr[i][0] + solve(arr, 0, i + 1,dp);
-            op2 = arr[i][1] + solve(arr, 1, i + 1,dp);
-        }
-
-
-        return dp[i][precol] = Math.max(op1, op2);
+        return dp[i][precol] = Math.max(op1 , op2);
     }
-
     public int maximumPoints(int arr[][]) {
-        
+        // code here
         int n = arr.length;
         int m = arr[0].length;
-         int [] [] dp = new int [n][m];
-        for (int [] row : dp) {
-            Arrays.fill(row , -1);
+        int [][] dp  = new int [n][m];
+        for (int [] row: dp){
+            Arrays.fill(row, -1);
         }
-        
+         
         int ans = -1;
         
         for(int j = 0; j < 3; j++){
-            ans = Math.max(ans, solve(arr, j, 0,dp));
+            ans = Math.max(ans, solve(arr, 0, j,dp));
         }
         
         return ans;
