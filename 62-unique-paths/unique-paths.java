9d51f42a3416e17ce1c5c8ee548e992312107fae
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-04 16:46:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (60.45%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -2,6 +2,7 @@ class Solution {
     public int solve (int m , int n , int i , int j , int [] []dp) {
         if (i >=m || j>= n) return 0;
         if (i == m-1 || j == n-1) return 1;
+        if (i == m-1 && j == n-1) return 0;
         if(dp [i][j] != -1) return dp[i][j];
         return dp[i][j] = solve(m , n , i+1 , j, dp) + solve(m, n , i , j+1 , dp);
     }
