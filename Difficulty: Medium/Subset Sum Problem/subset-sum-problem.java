5e9c3ffc23ddde9bba6df44b953f89df3147dd4e
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-04 19:43:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -29,23 +29,28 @@ class GFG {
 
 
 class Solution {
+    static int [][] dp;
+    static Boolean solve(int [] arr  , int sum , int idx) {
+        if(sum == 0) return true;
+        if(idx>= arr.length || sum <0) return false;
+        if(dp[idx][sum] != -1) return dp[idx][sum] == 1;
+
+        boolean take = solve(arr , (sum-arr[idx]) , idx+1);
+        boolean nottake = solve(arr , sum , idx+1);
+        dp[idx][sum] = (take || nottake) ? 1 : 0;
+        return take || nottake;
+
+        
+    }
 
     static Boolean isSubsetSum(int arr[], int sum) {
+        // code here
         int n = arr.length;
-        boolean [][] dp = new boolean[n+1][sum+1];
-        
-        for(int i=0;i<=n;i++) {
-            dp[i][0]=true;
+
+        dp = new int [n][sum+1];
+        for(int [] row : dp) {
+            Arrays.fill(row , -1);
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=sum;j++){
-                if(arr[i-1]>j){
-                    dp[i][j] = dp[i-1][j];
-                }else{
-                    dp[i][j] = dp[i-1][j]||dp[i-1][j-arr[i-1]];
-                }
-            }
-        }
-        return dp[n][sum];
+        return solve(arr , sum , 0);
     }
 }
