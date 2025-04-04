c878b2263afb24a8eef4ceb072f290f15d391d6b
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-04 14:19:39 +0530

    Time: 1 ms (99.85%) | Memory: 47.5 MB (69.85%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,24 +1,19 @@
 class Solution {
-
-    public int solve(int [][] grid , int i , int j , int [][] dp) {
-        int n = grid.length;
-        int m = grid[0].length;
-         if (i >= n || j >= m) {
-            return Integer.MAX_VALUE;
-        }
-        if(i == n-1 && j == m-1) return grid[i][j];
+    public int solve(int [][] grid , int m , int n  ,int i , int j,  int [][] dp) {
+        if(i >= m || j >= n) return Integer.MAX_VALUE; ;
+        if(i == m-1 && j == n-1) return grid[i][j];
         if (dp [i][j] != -1) return dp [i][j];
-        long  op1  = grid[i][j] + (long)solve(grid , i , j+1,  dp);
-        long  op2 = grid[i][j] + (long)solve(grid , i+1 , j,dp );
-        return dp[i][j]  =  (int)Math.min(op1,op2);
+        long op1 = grid[i][j] + (long)solve(grid , m , n , i, j+1 , dp) ;
+        long op2 = grid[i][j] + (long)solve(grid , m , n , i+1, j , dp) ;
+        return dp[i][j] = (int)Math.min(op1 , op2);
     }
     public int minPathSum(int[][] grid) {
-         int n = grid.length;
-        int m = grid[0].length;
-         int [] [] dp = new int [n][m];
+        int m = grid.length;
+        int n = grid[0].length;
+        int [][] dp = new int [m][n];
         for (int [] row : dp) {
             Arrays.fill(row , -1);
         }
-        return solve(grid , 0, 0 , dp);
+        return solve(grid ,m , n , 0 , 0 , dp );
     }
 }
