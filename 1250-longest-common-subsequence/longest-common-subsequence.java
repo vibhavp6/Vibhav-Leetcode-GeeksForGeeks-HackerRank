2e4f9e2c91a8d07a0294d7bccd218e7c2ed3a085
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-05 13:03:24 +0530

    Time: 32 ms (11.09%) | Memory: 50.7 MB (75.57%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,29 +1,30 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        // Lengths of the input strings
-        int length1 = text1.length();
-        int length2 = text2.length();
-      
-        // Create a 2D array to store the lengths of longest common subsequences
-        // for all subproblems, initialized with zero
-        int[][] dp = new int[length1 + 1][length2 + 1];
 
-        // Build the dp array from the bottom up
-        for (int i = 1; i <= length1; ++i) {
-            for (int j = 1; j <= length2; ++j) {
-                // If characters match, take diagonal value and add 1
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-                // If characters do not match, take the maximum value from 
-                // the left (dp[i][j-1]) or above (dp[i-1][j])
-                else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
+    public int solve(String a, String b, int i, int j, int[][] dp) {
+        // Base case
+        if (i == a.length()) return 0;
+        if (j == b.length()) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int ans = 0;
+        if (a.charAt(i) == b.charAt(j)) {
+            ans = 1 + solve(a, b, i + 1, j + 1, dp);
+        } else {
+            ans = Math.max(solve(a, b, i + 1, j, dp), solve(a, b, i, j + 1, dp));
+        }
+
+        dp[i][j] = ans;
+        return ans;
+    }
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length()][text2.length()];
+        for (int i = 0; i < text1.length(); i++) {
+            for (int j = 0; j < text2.length(); j++) {
+                dp[i][j] = -1;
             }
         }
-        // The bottom-right cell contains the length of the longest
-        // common subsequence of text1 and text2
-        return dp[length1][length2];
+        return solve(text1, text2, 0, 0, dp);
     }
 }
