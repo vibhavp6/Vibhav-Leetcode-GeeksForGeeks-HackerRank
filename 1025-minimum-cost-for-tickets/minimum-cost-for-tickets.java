072329632305544341ca9d6c79378c65eba6427b
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-06 13:52:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (57.56%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,25 +1,27 @@
 class Solution {
-    public int mincostTickets(int[] days, int[] costs) {
-        int maxDay = days[days.length - 1];
-        boolean[] travelDay = new boolean[maxDay + 1];
+    public int solve(int[] days, int[] costs, int index, int[] dp) {
+        if (index >= days.length) return 0;
+        if (dp[index] != -1) return dp[index];
 
-        for (int day : days)
-            travelDay[day] = true;
+        int oneday = costs[0] + solve(days, costs, index + 1, dp);
 
-        int[] dp = new int[maxDay + 1];
-        dp[0] = 0;
-
-        for (int i = 1; i <= maxDay; i++) {
-            if (!travelDay[i]) {
-                dp[i] = dp[i - 1];
-                continue;
-            }
-
-            dp[i] = costs[0] + dp[i - 1];
-            dp[i] = Math.min(dp[Math.max(0, i - 7)] + costs[1], dp[i]);
-            dp[i] = Math.min(dp[Math.max(0, i - 30)] + costs[2], dp[i]);
+        int sevvenday = Integer.MAX_VALUE;
+        for (int j = index; j < days.length && days[j] < days[index] + 7; j++) {
+            sevvenday = costs[1] + solve(days, costs, j + 1, dp);
         }
 
-        return dp[maxDay];
+        int thiday = Integer.MAX_VALUE;
+        for (int j = index; j < days.length && days[j] < days[index] + 30; j++) {
+            thiday = costs[2] + solve(days, costs, j + 1, dp);
+        }
+
+        dp[index] = Math.min(oneday, Math.min(sevvenday, thiday));
+        return dp[index];
+    }
+
+    public int mincostTickets(int[] days, int[] costs) {
+        int[] dp = new int[days.length];
+        Arrays.fill(dp, -1); 
+        return solve(days, costs, 0, dp);
     }
 }
