c9704c2d0c1be59272439876949fc1be198db39e
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-07 19:39:00 +0530

    Time: 6 ms (25.34%) | Memory: 52.4 MB (15.08%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,31 +1,43 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean [][] vis = new boolean[m][n];
+        int count = 0;
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++) {
+                if(grid[i][j] == '1' && (!vis[i][j])){
+                    count++;
+                    bfs(grid , i , j , vis);
                 }
             }
         }
-        
-        return numIslands;
+        return count;
     }
-    
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
-            return;
+
+    public void bfs(char [][] grid , int i , int j , boolean [][] vis) {
+        int n = grid.length;
+        int m = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int [] {i,j});
+        vis[i][j] = true ; 
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        while(!q.isEmpty()) {
+            int []node = q.poll();
+            int x = node[0];
+            int y = node[1];
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                if(nx >= 0 && nx < n  && ny >= 0 && ny < m){
+                    if(grid[nx][ny] == '1' && (!vis[nx][ny])){
+                        vis[nx][ny] = true;
+                        q.add(new int[]{nx, ny});
+                    }
+                }
+            }
         }
-        
-        grid[i][j] = '0'; // mark as visited
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
+
     }
 }
