876b0485312a5b88850f1ece8e9e97cbeb2b81ad
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-10 01:11:21 +0530

    Time: 18 ms (97.95%) | Memory: 83.5 MB (57.16%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.java
+++ 2035-count-sub-islands/count-sub-islands.java
@@ -1,44 +1,52 @@
 class Solution {
-    public int countSubIslands(int[][] grid1, int[][] grid2) {
-        int row = grid1.length;
-        int column = grid1[0].length;
-        int count = 0;
-        boolean [][] visit = new boolean[row][column];
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if (grid1[i][j] == 1 && grid2[i][j] == 1 && !visit[i][j]) {
-                    boolean isSubIsland = dfs(i, j, grid1, grid2, visit);
+    int m;  // Number of rows in the grid
+    int n;  // Number of columns in the grid
+
+    boolean isSubIsland;  // Flag to track if the current island in grid2 is a sub-island of grid1
+
+    // Depth-First Search (DFS) to explore the island in grid2
+    public void dfs(int[][] grid1, int[][] grid2, int i, int j) {
+        // If the current position is out of bounds or already visited (grid2[i][j] == 0), return
+        if (i < 0 || i == m || j < 0 || j == n || grid2[i][j] == 0) return;
+
+        // If the current cell in grid2 is land but not in grid1, mark this island as not a sub-island
+        if (grid1[i][j] != grid2[i][j]) {
+            isSubIsland = false;
+        }
+
+        // Mark the current cell as visited by setting grid2[i][j] to 0
+        grid2[i][j] = 0;
+
+        // Continue the DFS in all four directions
+        dfs(grid1, grid2, i + 1, j);  // Down
+        dfs(grid1, grid2, i - 1, j);  // Up
+        dfs(grid1, grid2, i, j + 1);  // Right
+        dfs(grid1, grid2, i, j - 1);  // Left
+    }
+
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        m = grid1.length;    // Number of rows in the grid
+        n = grid1[0].length; // Number of columns in the grid
+
+        int subIslandCount = 0;  // To store the count of sub-islands
+
+        // Iterate through each cell in grid2
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // If the cell is land in grid2, it could be part of a sub-island
+                if (grid2[i][j] == 1) {
+                    isSubIsland = true;  // Start assuming this is a sub-island
+                    dfs(grid1, grid2, i, j);  // Perform DFS to check the whole island
+
+                    // If the flag remains true after DFS, it means this island is a sub-island
                     if (isSubIsland) {
-                        count++;
+                        subIslandCount++;  // Increment the count of sub-islands
                     }
                 }
             }
         }
-        return count;
-    }
 
-    public boolean dfs(int i, int j, int[][] grid1, int[][] grid2, boolean[][] visit) {
-        int n = grid1.length;
-        int m = grid1[0].length;
-
- 
-        if (i < 0 || i >= n || j < 0 || j >= m || grid2[i][j] == 0) {
-            return true; 
-        }
-
-        grid2[i][j] = 0;
-
-        // Check if the current island in grid2 is part of an island in grid1
-        boolean isSubIsland = grid1[i][j] == 1;
-
-        // Explore all 4 directions (up, down, left, right)
-        if (i - 1 >= 0) isSubIsland &= dfs(i - 1, j, grid1, grid2, visit);
-        if (i + 1 < n) isSubIsland &= dfs(i + 1, j, grid1, grid2, visit);
-        if (j - 1 >= 0) isSubIsland &= dfs(i, j - 1, grid1, grid2, visit);
-        if (j + 1 < m) isSubIsland &= dfs(i, j + 1, grid1, grid2, visit);
-
-        // Return if it's a valid sub-island
-        return isSubIsland;
+        return subIslandCount;  // Return the total count of sub-islands
     }
 }
