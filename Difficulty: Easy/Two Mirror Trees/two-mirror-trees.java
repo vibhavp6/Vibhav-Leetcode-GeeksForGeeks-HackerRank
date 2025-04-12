d238d70fa6fc94b907ebc7ee2952d72d8d7b4064
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-12 12:57:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Mirror Trees/two-mirror-trees.java
@@ -0,0 +1,127 @@
+//{ Driver Code Starts
+// INITIAL CODE
+
+// Contributed by Sudarshan Sharma
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            String s1 = br.readLine();
+            Node root = buildTree(s);
+            Node root1 = buildTree(s1);
+            Solution g = new Solution();
+            if (g.areMirror(root, root1)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// FUNCTION CODE
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    boolean areMirror(Node a, Node b) {
+        // Your code here
+         boolean ans = helper(a,b);
+        return ans;
+    }
+    public boolean helper(Node p , Node q){
+        if(p == null && q == null) return true;
+        if(p == null|| q == null) return false;
+        return (p.data == q.data) &&helper(p.left  , q.right) && helper(p.right, q.left);    
+    }
+}
