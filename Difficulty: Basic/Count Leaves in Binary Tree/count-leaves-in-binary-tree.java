4a2c7f31b9a9a259386e385fee3596fb2a5de004
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-12 13:04:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count Leaves in Binary Tree/count-leaves-in-binary-tree.java
+++ Difficulty: Basic/Count Leaves in Binary Tree/count-leaves-in-binary-tree.java
@@ -1,114 +1,112 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
-import java.util.LinkedList; 
-import java.util.Queue; 
 import java.io.*;
 import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class Node{
+class Node {
     int data;
     Node left;
     Node right;
-    Node(int data){
+
+    Node(int data) {
         this.data = data;
-        left=null;
-        right=null;
+        left = null;
+        right = null;
     }
 }
 
 class GfG {
-    
-    static Node buildTree(String str){
-        
-        if(str.length()==0 || str.charAt(0)=='N'){
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
             return null;
         }
-        
+
         String ip[] = str.split(" ");
         // Create the root of the tree
         Node root = new Node(Integer.parseInt(ip[0]));
         // Push the root to the queue
-        
-        Queue<Node> queue = new LinkedList<>(); 
-        
+
+        Queue<Node> queue = new LinkedList<>();
+
         queue.add(root);
         // Starting from the second element
-        
+
         int i = 1;
-        while(queue.size()>0 && i < ip.length) {
-            
+        while (queue.size() > 0 && i < ip.length) {
+
             // Get and remove the front of the queue
             Node currNode = queue.peek();
             queue.remove();
-                
+
             // Get the current node's value from the string
             String currVal = ip[i];
-                
+
             // If the left child is not null
-            if(!currVal.equals("N")) {
-                    
+            if (!currVal.equals("N")) {
+
                 // Create the left child for the current node
                 currNode.left = new Node(Integer.parseInt(currVal));
                 // Push it to the queue
                 queue.add(currNode.left);
             }
-                
+
             // For the right child
             i++;
-            if(i >= ip.length)
-                break;
-                
+            if (i >= ip.length) break;
+
             currVal = ip[i];
-                
+
             // If the right child is not null
-            if(!currVal.equals("N")) {
-                    
+            if (!currVal.equals("N")) {
+
                 // Create the right child for the current node
                 currNode.right = new Node(Integer.parseInt(currVal));
-                    
+
                 // Push it to the queue
                 queue.add(currNode.right);
             }
             i++;
         }
-        
+
         return root;
     }
-    static void printInorder(Node root)
-    {
-        if(root == null)
-            return;
-            
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
         printInorder(root.left);
-        System.out.print(root.data+" ");
-        
+        System.out.print(root.data + " ");
+
         printInorder(root.right);
     }
-    
-	public static void main (String[] args) throws IOException{
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        
-	        int t=Integer.parseInt(br.readLine());
-    
-	        while(t > 0){
-	            String s = br.readLine();
-    	    	Node root = buildTree(s);
-    	    	
-                Tree g = new Tree();
-        		System.out.println(g.countLeaves(root));
-                    t--;
-            
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+
+            Solution g = new Solution();
+            System.out.println(g.countLeaves(root));
+            t--;
+
+            System.out.println("~");
         }
     }
 }
 
-
-
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
 /* A Binary Tree node
 class Node
@@ -117,12 +115,17 @@ class Node
     Node left, right;
 }*/
 
-class Tree
-{
-    int countLeaves(Node node) 
-    {
-        if (node == null) return 0;
-        if (node.left == null && node.right == null) return 1 ;
-        return countLeaves(node.left) + countLeaves(node.right) ;
+class Solution {
+    int cnt = 0;
+    int countLeaves(Node node) {
+        // Your code
+        count(node);
+        return cnt;
+    }
+    public void count(Node node){
+        if(node == null ) return ;
+        if(node.left == null && node.right == null) cnt++;;
+        count(node.left);
+        count(node.right);
     }
 }
