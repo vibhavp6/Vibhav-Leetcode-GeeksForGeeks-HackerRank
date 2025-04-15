89dd49294db0aacd0c0cf7e107220ace0aa379fb
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-15 18:36:38 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (47.52%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -13,23 +13,49 @@
  *     }
  * }
  */
-class Solution {
+// class Solution {
+//     public int kthSmallest(TreeNode root, int k) {
+//         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+//         inorder(root, k, maxHeap);
+//         return maxHeap.peek();
+//     }
+
+//     private void inorder(TreeNode root, int k, PriorityQueue<Integer> maxHeap) {
+//         if (root == null) return;
+
+//         inorder(root.left, k, maxHeap);
+
+//         maxHeap.offer(root.val);
+//         if (maxHeap.size() > k) {
+//             maxHeap.poll();
+//         }
+
+//         inorder(root.right, k, maxHeap);
+//     }
+// }
+
+    // m-2 
+
+    class Solution {
+    private int count = 0;
+    private int ans = -1;
+
     public int kthSmallest(TreeNode root, int k) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        inorder(root, k, maxHeap);
-        return maxHeap.peek();
+        inorder(root, k);
+        return ans;
     }
 
-    private void inorder(TreeNode root, int k, PriorityQueue<Integer> maxHeap) {
-        if (root == null) return;
+    private void inorder(TreeNode root, int k) {
+        if (root == null || count >= k) return;
 
-        inorder(root.left, k, maxHeap);
+        inorder(root.left, k);
 
-        maxHeap.offer(root.val);
-        if (maxHeap.size() > k) {
-            maxHeap.poll();
+        count++;
+        if (count == k) {
+            ans = root.val;
+            return;
         }
 
-        inorder(root.right, k, maxHeap);
+        inorder(root.right, k);
     }
 }
