52fcff0161d63e957538eec6ceddbb0e9375c966
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-15 19:27:09 +0530

    Time: 2 ms (95.22%) | Memory: 45.2 MB (61.64%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -13,24 +13,41 @@
  *     }
  * }
  */
+// class Solution {
+//     public void inorder(TreeNode root , ArrayList<Integer> ans ){
+//         if(root == null) return ;
+//         inorder(root.left , ans);
+//         ans.add(root.val);
+//         inorder(root.right, ans);
+//     }
+//     public boolean findTarget(TreeNode root, int k) {
+//         ArrayList<Integer> ans = new ArrayList<>();
+//         inorder(root, ans);
+//         HashSet<Integer> st = new HashSet<>();
+//         for(int num : ans) {
+//             int diff = k - num;
+//             if(st.contains(diff)){
+//                 return true;
+//             }
+//             st.add(num);
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
-    public void inorder(TreeNode root , ArrayList<Integer> ans ){
-        if(root == null) return ;
-        inorder(root.left , ans);
-        ans.add(root.val);
-        inorder(root.right, ans);
-    }
     public boolean findTarget(TreeNode root, int k) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        inorder(root, ans);
-        HashSet<Integer> st = new HashSet<>();
-        for(int num : ans) {
-            int diff = k - num;
-            if(st.contains(diff)){
-                return true;
-            }
-            st.add(num);
-        }
-        return false;
+        HashSet<Integer> seen = new HashSet<>();
+        return dfs(root, k, seen);
+    }
+
+    private boolean dfs(TreeNode node, int k, HashSet<Integer> seen) {
+        if (node == null) return false;
+
+        if (seen.contains(k - node.val)) return true;
+
+        seen.add(node.val);
+
+        return dfs(node.left, k, seen) || dfs(node.right, k, seen);
     }
 }
