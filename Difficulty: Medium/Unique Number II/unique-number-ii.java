af3b84a8bdbca42e8ffed9a551cdbc4495fc46b4
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-04-23 14:17:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Number II/unique-number-ii.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.HashMap;
+
+
+// } Driver Code Ends
+
+class Solution {
+    public int[] singleNum(int[] arr) {
+        int xor=0;
+        for(int a: arr)xor^=a;
+        int x=0,y=0;
+        int lx=xor&(-xor);
+        
+        for(int a: arr){
+            if((a&lx)==0) x^=a;
+            else y^=a;
+        }
+        return new int[]{Math.min(x,y),Math.max(x,y)};
+    }
+}
+
+
+//{ Driver Code Starts.
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            // int k = Integer.parseInt(br.readLine());
+            // Create Solution object and find closest sum
+            Solution ob = new Solution();
+            int[] ans = ob.singleNum(arr);
+
+            // Print the result as a space-separated string
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println(); // New line after printing the results
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
