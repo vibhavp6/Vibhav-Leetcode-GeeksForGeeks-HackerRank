7cf026ebf4afaf9a4af7b5c5d3aa7def04bc15ff
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-05-08 01:00:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Missing element of AP/missing-element-of-ap.java
+++ Difficulty: Medium/Missing element of AP/missing-element-of-ap.java
@@ -14,6 +14,8 @@ public class Main {
             int[] arr = Arrays.stream(parts).mapToInt(Integer::parseInt).toArray();
 
             System.out.println(solution.findMissing(arr));
+
+            System.out.println("~");
         }
     }
 }
@@ -24,19 +26,21 @@ public class Main {
 // User function Template for Java
 
 class Solution {
-    int findMissing(int[] arr) {
+    public int findMissing(int[] arr) {
         int n = arr.length;
-        int first = arr[0];
-        int last = arr[n-1];
-        int num = 0;
-        int diff = (last - first) / n;
-
-        for (int i = 1; i < n; i++) {
-            if (arr[i] != (arr[i-1] + diff)) {
-                num = arr[i-1] + diff;
-                break;
+       
+        int prevDiff = arr[1] - arr[0];
+        
+      
+        for (int i = 2; i < n; i++) {
+            int currDiff = arr[i] - arr[i - 1];
+            if (currDiff != prevDiff) {
+               
+                return arr[i - 1] + prevDiff;
             }
         }
-        return num;
+        
+       
+        return arr[n - 1] + prevDiff;
     }
 }
