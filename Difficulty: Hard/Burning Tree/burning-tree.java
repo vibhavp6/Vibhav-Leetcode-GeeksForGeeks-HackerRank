b79f5743d3dd33f5fc8d77fe57a854ba03bc7f65
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-05-20 15:21:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Burning Tree/burning-tree.java
+++ Difficulty: Hard/Burning Tree/burning-tree.java
@@ -1,162 +1,78 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-
-import java.util.LinkedList;
-import java.util.Queue;
-import java.io.*;
-import java.util.*;
-
+/*
 class Node {
-	int data;
-	Node left;
-	Node right;
+    int data;
+    Node left;
+    Node right;
 
-	Node(int data) {
-		this.data = data;
-		left = null;
-		right = null;
-	}
-}
-
-class GfG {
-
-	static Node buildTree(String str) {
-
-		if (str.length() == 0 || str.charAt(0) == 'N') {
-			return null;
-		}
-
-		String ip[] = str.split(" ");
-		// Create the root of the tree
-		Node root = new Node(Integer.parseInt(ip[0]));
-		// Push the root to the queue
-
-		Queue<Node> queue = new LinkedList<>();
-
-		queue.add(root);
-		// Starting from the second element
-
-		int i = 1;
-		while (queue.size() > 0 && i < ip.length) {
-
-			// Get and remove the front of the queue
-			Node currNode = queue.peek();
-			queue.remove();
-
-			// Get the current node's value from the string
-			String currVal = ip[i];
-
-			// If the left child is not null
-			if (!currVal.equals("N")) {
-
-				// Create the left child for the current node
-				currNode.left = new Node(Integer.parseInt(currVal));
-				// Push it to the queue
-				queue.add(currNode.left);
-			}
-
-			// For the right child
-			i++;
-			if (i >= ip.length)
-				break;
-
-			currVal = ip[i];
-
-			// If the right child is not null
-			if (!currVal.equals("N")) {
-
-				// Create the right child for the current node
-				currNode.right = new Node(Integer.parseInt(currVal));
-
-				// Push it to the queue
-				queue.add(currNode.right);
-			}
-			i++;
-		}
-
-		return root;
-	}
-
-	static void printInorder(Node root) {
-		if (root == null)
-			return;
-
-		printInorder(root.left);
-		System.out.print(root.data + " ");
-
-		printInorder(root.right);
-	}
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int t = Integer.parseInt(br.readLine());
-
-		while (t > 0) {
-			String s = br.readLine();
-			int target = Integer.parseInt(br.readLine());
-			Node root = buildTree(s);
-
-			Solution g = new Solution();
-			System.out.println(g.minTime(root, target));
-			t--;
-
-		}
-	}
-}
-
-
-
-// } Driver Code Ends
-
-
-//User function Template for Java
-class Solution {
-    static int BFS(Map<Integer, HashSet<Integer>> adj, int src) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        Set<Integer> visited = new HashSet<>();
-        int count = 0;
-        
-        queue.offer(src);
-        visited.add(src);
-        
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            count++;
-            for (int i = 0; i < size; i++) {
-                int node = queue.poll();
-                for (int neighbor : adj.get(node)) {
-                    if (!visited.contains(neighbor)) {
-                        visited.add(neighbor);
-                        queue.offer(neighbor);
-                    }
-                }
-            }
-        }
-        return count - 1;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
     }
-    
-    static void createGraph(Node root, Map<Integer, HashSet<Integer>> adj) {
-        if (root == null) return;
-        
-        if (root.left != null) {
-            adj.computeIfAbsent(root.data, k -> new HashSet<>()).add(root.left.data);
-            adj.computeIfAbsent(root.left.data, k -> new HashSet<>()).add(root.data);
-            createGraph(root.left, adj);
+}  */
+class Solution {
+
+    static HashMap<Node, Integer> parentDist;
+    static int res;
+
+    /**
+     * Mark the possible parents of the target nodes with there distance.
+     * since only the parents are getting stored. the maximum space it can take
+     * is O(n) when tree is skewed and average O(log n) when tree is balanced
+     */
+    static int parentMark(Node root, int target) {
+        if (root == null) {
+            return -1;
         }
-        
-        if (root.right != null) {
-            adj.computeIfAbsent(root.data, k -> new HashSet<>()).add(root.right.data);
-            adj.computeIfAbsent(root.right.data, k -> new HashSet<>()).add(root.data);
-            createGraph(root.right, adj);
+
+        if (root.data == target) {
+            parentDist.put(root, 0);
+            return 0;
+        }
+
+        int left = parentMark(root.left, target);
+        if (left != -1) {
+            parentDist.put(root, left + 1);
+            return left + 1;
+        }
+
+        int right = parentMark(root.right, target);
+        if (right != -1) {
+            parentDist.put(root, right + 1);
+            return right + 1;
+        }
+
+        return -1;
+    }
+
+    /**
+     * Simply this is finding shortest distance from 
+     * the target node and taking maximum of it 
+     * if the node is present as parent node then record its distance from map and 
+     * move downward with incremented distance of current node.
+     * if node is not parent then use current depth as the distance and move downwards
+     */
+    static void maxDist(Node root, int depth) {
+        if (root == null) {
+            return;
+        }
+
+        if (parentDist.containsKey(root)) {
+            res = Math.max(res, parentDist.get(root));
+            maxDist(root.left, parentDist.get(root) + 1);
+            maxDist(root.right, parentDist.get(root) + 1);
+        } else {
+            res = Math.max(res, depth);
+            maxDist(root.left, depth + 1);
+            maxDist(root.right, depth + 1);
         }
     }
 
     public static int minTime(Node root, int target) {
-        if(root.left == null && root.right == null) return 0;
-        Map<Integer, HashSet<Integer>> adj = new HashMap<>();
-        createGraph(root, adj);
-        return BFS(adj, target);
+        parentDist = new HashMap<>();
+        parentMark(root, target);
+        res = 0;
+        maxDist(root, 0);
+        return res;
     }
 }
