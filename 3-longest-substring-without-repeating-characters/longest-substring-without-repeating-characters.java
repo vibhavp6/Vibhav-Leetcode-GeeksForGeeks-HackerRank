bbc5be5302724e1920a5c6030ea316e8936a69d5
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-06-05 13:19:08 +0530

    Time: 9 ms (17.35%) | Memory: 45 MB (32.43%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,10 +1,10 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         HashMap<Character , Integer> ans = new HashMap<>();
-        int st = 0;
+        int st = 0; 
         int length = 0;
-        for(int i = 0; i<s.length() ; i++){
-            ans.put(s.charAt(i) , ans.getOrDefault(s.charAt(i) , 0) +1);
+        for (int i = 0; i<s.length(); i++) {
+            ans.put(s.charAt(i) , ans.getOrDefault(s.charAt(i) , 0) +1) ;
             while(ans.get(s.charAt(i))>1){
                 ans.put(s.charAt(st) , ans.get(s.charAt(st)) -1);
                 if(ans.get(s.charAt(st)) ==0){
@@ -15,5 +15,6 @@ class Solution {
             length = Math.max(length,i-st+1);
         }
         return length;
+        
     }
 }
