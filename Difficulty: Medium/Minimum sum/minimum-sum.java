2f8cadf0da816132f9d2fabc6adbac6872281f79
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-06-23 19:34:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum sum/minimum-sum.java
+++ Difficulty: Medium/Minimum sum/minimum-sum.java
@@ -1,56 +1,39 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String arr[] = br.readLine().split(" ");
-            int a[] = new int[arr.length];
-
-            for (int i = 0; i < arr.length; i++) {
-                a[i] = Integer.parseInt(arr[i]);
-            }
-            Solution obj = new Solution();
-            int f = 0;
-            String A = obj.minSum(a);
-            System.out.println(A);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
     String minSum(int[] arr) {
         // code here
-        Arrays.sort(arr);
-         StringBuilder st=new StringBuilder();
-        int carry=0;
-        for(int i=arr.length-1;i>=0;i-=2){
-            if(arr[i]+carry==0) break;
-            if(i-1>=0){
-                if(arr[i]+arr[i-1]+carry>9){
-                    st.append((arr[i]+arr[i-1]+carry)%10);
-                     carry=1;
-                }
-                else {st.append(arr[i]+arr[i-1]+carry); carry=0;}
-            }
-            else{
-                    st.append(carry+arr[i]);
-                    carry=0;
-            }
-        }
-        if(carry>0) st.append(carry);
-        return st.reverse().toString();
+        int n = arr.length;
         
+        Arrays.sort(arr);
+        
+        int start = 0;
+        // To avoid multiple zeroes in arr
+        while(start < n && arr[start] == 0) {
+            start++;
+        }
+        
+        
+        StringBuilder res = new StringBuilder();
+        
+        int i=n-1;
+        int sum, carry = 0;
+        while(i>start) {
+            sum = arr[i] + arr[i-1] + carry;
+            res.append(sum % 10);
+            
+            carry = sum/10;
+            
+            i-=2;
+        }
+        if(i == start) {
+            sum = arr[start] + carry;
+            res.append(sum%10);
+            carry = sum/10;
+        }
+        if(carry > 0) {
+            res.append(carry);
+        }
+        
+        res.reverse();
+        return res.toString();
     }
 }
