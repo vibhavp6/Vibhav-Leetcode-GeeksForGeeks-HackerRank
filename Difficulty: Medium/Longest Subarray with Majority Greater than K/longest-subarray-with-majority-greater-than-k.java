c97bd937f7a3feb0cfe8ae37eef0134442e9c3ee
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-08-02 11:56:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
@@ -1,12 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
-
-// User function Template for Java
 class Solution {
     static int longestSubarray(int[] arr, int k) {
         // Code Here
@@ -31,34 +22,3 @@ class Solution {
         return maxLen;
     }
 }
-
-
-
-//{ Driver Code Starts.
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-            int n = tokens.length;
-            int[] arr = new int[n];
-
-            int i = 0;
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                arr[i] = Integer.parseInt(token);
-                i++;
-            }
-
-            int k = Integer.parseInt(br.readLine().trim());
-            System.out.println(new Solution().longestSubarray(arr, k));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
