6663656317bf33327fb58d99a86b1c1b57e8d91d
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-08-23 12:35:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
@@ -1,76 +1,45 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.findPages(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
-//Back-end complete function Template for Java
-
 class Solution {
-    public static int check (long mid, int []arr ,int k) {
-        long partition = 1;
-        long sum = 0;
-        for(int i = 0; i<arr.length;i++){
-            if (sum+arr[i] > mid) {
-                partition ++;
-                sum = arr[i];
-            } 
-            else {
-              sum +=arr[i];
+     public static int countStudent(int []arr,int pages){
+        int n = arr.length;
+        int student=1;
+        long studentPages=0;
+        for(int i =0;i<n;i++){
+            if(studentPages+arr[i]<=pages){
+                studentPages+=arr[i];
+            }else{
+                student++;
+                studentPages=arr[i];
             }
-            if (partition>k) return 1;
         }
-        return 0;
+        return student;
     }
     public static int findPages(int[] arr, int k) {
-        // code here
-        if(arr.length < k) return -1;
-        long left = -1 , right = -1 , maxval = arr[0] , sum = 0 ,ans = -1;
-        for (int i = 0; i<arr.length; i++){
-            sum += arr[i];
-            if (arr[i] > maxval) {
-                maxval = arr[i];
-            }
+        int n =arr.length;
+         if(k>n){
+            return -1;
         }
-        left = maxval;
-        right = sum;
-        while(left<=right){
-            long mid  = left + (right-left)/2;
-            int flag = check(mid,arr,k);
-            if(flag == 1) {
-                left = mid +1;
-            }
-            else {
-                ans = mid;
-                right = mid -1;
-            }
-        }
-        return (int)ans;
+        int low = arr[0];  
+
+for (int i = 1; i < arr.length; i++) {
+    if (arr[i] > low) {
+        low = arr[i];  
     }
 }
+        int high=0;
+        for(int  i =0;i<n;i++){
+            high+=arr[i];
+
+        }
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(countStudent(arr, mid)>k){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return low;
+        
+    }
+}
+
