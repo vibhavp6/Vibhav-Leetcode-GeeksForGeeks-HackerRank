3e9095d75f6c2cb1f09a9e98f803822e465c9be6
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-08-30 11:01:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
@@ -1,59 +1,26 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t > 0) {
-            int N = sc.nextInt();
-            int M[][] = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    M[i][j] = sc.nextInt();
-                }
-            }
-            System.out.println(new Solution().celebrity(M));
-            t--;
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    // Function to find if there is a celebrity in the party or not.
     public int celebrity(int mat[][]) {
-        for (int i = 0; i < mat.length; i++) {
-            //Number of people known by each person(row)
-            int numPeopleKnown = 0; 
-            
-            for (int j = 0; j < mat[0].length; j++) {
-                if (mat[i][j] == 1) {
-                    numPeopleKnown += 1;
-                }
-            }
-            
-            //Celebrity knows no one (numPeopleKnown == 0)
-            if (numPeopleKnown == 0){
-                for (int j = 0; j < mat.length; j++){
-                    //Excluding celebrity row
-                    if (j != i) {
-                        //If someone doesn't know celebrity
-                        if (mat[j][i] == 0) {
-                            return -1;
-                        }
-                    }
-                }
-                return i;
-            }
-        }
-        
-        return -1;
+        // code here
+        int n=mat.length;
+       Stack<Integer>st=new Stack<>();
+       for(int i=0;i<n;i++)
+       {
+           st.push(i);
+       }
+       while(st.size()>1)
+       {
+           int a=st.pop();
+           int b=st.pop();
+           if(mat[a][b]==1)st.push(b);
+           else st.push(a);
+       }
+       
+       int celeb=st.pop();
+       for(int i=0;i<n;i++)
+       {
+           if(i!=celeb && (mat[i][celeb]==0 || mat[celeb][i]==1))return -1;
+       }
+        return celeb;
     }
+  
 }
