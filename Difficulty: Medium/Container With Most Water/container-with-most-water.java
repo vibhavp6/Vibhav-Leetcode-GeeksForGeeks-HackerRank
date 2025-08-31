a8ee55a3fc0c4dd0d442e33674682e369b607625
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-08-31 11:29:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.java
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.java
@@ -1,47 +1,15 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Sorting {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().maxWater(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-
     public int maxWater(int arr[]) {
-        // Code Hereint maxWater(vector<int> &arr) {
-        // code here
-         if(arr.length==1){
-            return 0;
+        // Code Here
+        int n = arr.length;
+        int l = 0, r = n-1,res = 0;
+        while(l<r){
+            int a = arr[l];
+            int b = arr[r];
+            res = Math.max(res,(r-l) * Math.min(a,b));
+            if(a<b)l++;
+            else r--;
         }
-        int i=0;int j=arr.length-1;
-        int res=0;
-        while(i<j){
-            int area=Math.min(arr[i],arr[j])*(j-i);
-            res=Math.max(res,area);
-            if(arr[i]>arr[j]){
-                j--;
-            }else if(arr[i]<arr[j]){
-                i++;
-            }else{
-                i++; j--;
-            }
-        }
-        
         return res;
     }
 }
