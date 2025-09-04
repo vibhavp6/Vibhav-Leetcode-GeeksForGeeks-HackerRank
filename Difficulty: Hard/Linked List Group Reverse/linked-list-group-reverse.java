8a9db1ff1ae8921b402d3856778277344af1903c
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-09-04 10:51:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.java
+++ Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.java
@@ -1,63 +1,4 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int key) {
-        data = key;
-        next = null;
-    }
-}
-
-class ReverseInSize {
-    static Node head;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-
-        int t = Integer.parseInt(in.readLine().trim());
-
-        while (t-- > 0) {
-
-            String s[] = in.readLine().trim().split(" ");
-            int a1 = Integer.parseInt(s[0]);
-            Node head = new Node(a1);
-            Node tail = head;
-            for (int i = 1; i < s.length; i++) {
-                int a = Integer.parseInt(s[i]);
-                // addToTheLast(new Node(a));
-                tail.next = new Node(a);
-                tail = tail.next;
-            }
-
-            int k = Integer.parseInt(in.readLine().trim());
-            Solution ob = new Solution();
-            Node res = ob.reverseKGroup(head, k);
-            printList(res, out);
-            out.println();
-
-            out.println("~");
-        }
-        out.close();
-    }
-
-    public static void printList(Node node, PrintWriter out) {
-        while (node != null) {
-            out.print(node.data + " ");
-            node = node.next;
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-/*node class of the linked list
+/*
 class Node
 {
     int data;
@@ -68,44 +9,35 @@ class Node
         next = null;
     }
 }
-
 */
 
 class Solution {
     public static Node reverseKGroup(Node head, int k) {
         // code here
-        Node outerTemp = head;
-        Node innerTemp = head;
-        Node connectingNode = null;
-        Node newHead = null;
+        if(head==null) return head;
         
-        while(outerTemp != null){
-            int i = k;
-            while(i > 0 && innerTemp != null){
-                innerTemp = innerTemp.next;
-                i--;
-            }
-            
-            Node curr = outerTemp;
-            Node nextNode = curr;
-            Node prev = null;
-            while(curr != innerTemp){
-                nextNode = curr.next;
-                curr.next = prev;
-                prev = curr;
-                curr = nextNode;
-            }
-            
-            if(newHead == null) newHead = prev;
-            
-            if(connectingNode != null){
-                connectingNode.next = prev;
-            }
-            
-            connectingNode = outerTemp;
-            outerTemp = innerTemp;
+        Node Prev=null, Next=null, finalHead=null,PrevNode=null;
+        Node curr=head; boolean flag=true;
+        while (curr!=null)
+        {
+        int count=0;
+        Node last=curr; Prev=null;
+         while(curr !=null && count<k)
+         {
+             Next=curr.next;
+             curr.next=Prev;
+             Prev=curr;
+             curr=Next;
+             count++;
+         }
+        
+          if(flag)
+          {finalHead=Prev; flag=false;}
+          else{PrevNode.next=Prev;}
+          
+          PrevNode=last;
         }
-        
-        return newHead;
+      
+        return finalHead;
     }
 }
