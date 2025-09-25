3326b2d18b1e5fb48953941278829540cf3d32d6
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-09-25 13:00:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.java
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -1,86 +1,38 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            System.out.println(new Solution().minJumps(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    static int minJumps(int[] arr) {
+    public int minJumps(int[] arr) {
+        // code here
         int n = arr.length;
-        
-        // If there are less than two elements, no jumps are needed
-        if (n <= 1) {
-            return 0;
-        }
-        
-        // If the first element is 0, it's not possible to move forward
-        if (arr[0] == 0) {
+        if (n == 0 || arr[0] == 0) {
             return -1;
         }
-        
-        // Initialize variables
-        int jumps = 1; // Start from the first jump
-        int maxReach = arr[0]; // The farthest we can reach
-        int steps = arr[0]; // The number of steps we can take
-        
-        // Traverse the array
-        for (int i = 1; i < n; i++) {
-            // If we reached the end of the array
-            if (i == n - 1) {
-                return jumps;
-            }
-            
-            // Update the maxReach
-            maxReach = Math.max(maxReach, i + arr[i]);
-            
-            // Decrease steps
-            steps--;
-            
-            // If steps become 0, we need to increase the jump count
-            if (steps == 0) {
-                jumps++;
-                
-                // If we can't move forward, return -1
-                if (i >= maxReach) {
-                    return -1;
-                }
-                
-                // Reset steps to the number of steps we can take from the current position
-                steps = maxReach - i;
-            }
+        if (n == 1) {
+            return 1;
         }
-        
-        return -1; // This line will never be reached if the solution is possible
+        int step = 0, position = 0, maxReach = 0, indexForMaxReach = -1, range;
+        while (position<n) {
+            maxReach = 0;
+            range = arr[position];
+            if ((position+range)>=(n-1)) {
+                step++;
+                break;
+            }
+            for (int i=1; i<=arr[position]; i++) {
+                if ((position+i)<n && maxReach<(position+i+arr[position+i])) {
+                    maxReach= position+i+arr[position+i];
+                    indexForMaxReach = position+i;
+                }
+            }
+            step++;
+            position=indexForMaxReach;
+            if ((position+1)>=n) {
+                break;
+            }
+            if (maxReach == 0) {
+                step = -1;
+                break;
+            }
+            //System.out.println("Step=" + step + " Position=" + position);
+        }
+        return step;
     }
 }
