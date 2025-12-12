904a576d820454371a4624612b23cbcf6fe38812
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-12-12 08:42:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.java
+++ Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.java
@@ -1,50 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        int t = Integer.parseInt(in.readLine().trim());
-        while (t-- > 0) {
-            int n = Integer.parseInt(in.readLine().trim());
-            int a[][] = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                String s[] = in.readLine().trim().split(" ");
-                for (int j = 0; j < n; j++) {
-                    a[i][j] = Integer.parseInt(s[j]);
-                }
-            }
-            Solution ob = new Solution();
-            ob.transpose(n, a);
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    out.print(a[i][j] + " ");
-                }
-                out.println();
-            }
-
-            out.println("~");
-        }
-        out.close();
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-    public void transpose(int n, int mat[][]) {
+    public ArrayList<ArrayList<Integer>> transpose(int[][] mat) {
+        // code here
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         
-        for(int i = 0;i < n;i++){
-            for(int j = i ;j < n;j++){
-                int temp = mat[i][j];
-                mat[i][j] = mat[j][i];
-                mat[j][i] = temp;
+        for(int i = 0; i < mat.length; i++){
+            ArrayList<Integer> temp = new ArrayList<>();
+            for(int j = 0; j < mat[0].length; j++){
+                temp.add(mat[j][i]);
             }
+            list.add(temp);
         }
+        
+        return list;
     }
 }
