333d294cef83f99e917b8961e730f44789d0a559
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-12-26 12:09:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
@@ -1,59 +1,26 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.kthMissing(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     public int kthMissing(int[] arr, int k) {
-        int i=0;
-    	int j=arr.length-1;
-    	while(i<=j) {
-    		int mid=(i+j)>>1;
-    		if(arr[mid]-(mid+1)<k) {
-    			i=mid+1;
-    		}else {
-    			j=mid-1;
-    		}
-    	}
-    	if(j<0) {
-    		return k;
-    	}else if(j==arr.length-1) {
-    		int temp=arr[j];
-    		int miss=temp-(j+1);
-    		int needtoadd=k-miss;
-    		return temp+needtoadd;
-    		
-    	}else {
-    		int temp=arr[j];
-    		int miss=temp-(j+1);
-    		int needtoadd=k-miss;
-    		return temp+needtoadd;
-    	}
+        int n = arr.length;
+
+        // If kth missing is beyond the last element
+        if (arr[n - 1] - n < k) {
+            return arr[n - 1] + (k - (arr[n - 1] - n));
+        }
+
+        int low = 0, high = n - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        // kth missing number
+        return low + k;
     }
 }
