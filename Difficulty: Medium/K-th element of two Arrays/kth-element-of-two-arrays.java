be2c446b9c36a4007bcc4f7411f6b0958890ce8f
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2025-12-29 11:34:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -1,53 +1,17 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int k = Integer.parseInt(br.readLine().trim());
-
-            String[] line1 = br.readLine().trim().split(" ");
-            int[] a = new int[line1.length];
-            for (int i = 0; i < line1.length; i++) {
-                a[i] = Integer.parseInt(line1[i]);
-            }
-
-            String[] line2 = br.readLine().trim().split(" ");
-            int[] b = new int[line2.length];
-            for (int i = 0; i < line2.length; i++) {
-                b[i] = Integer.parseInt(line2[i]);
-            }
-
-            Solution ob = new Solution();
-            System.out.println(ob.kthElement(a, b, k));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     public int kthElement(int a[], int b[], int k) {
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        
-        for(int i :b){
-            arr.add(i);
+        // code here
+        List<Integer> set = new ArrayList<>();
+        for (int i=0;i<a.length;i++) {
+            set.add(a[i]);
         }
-        for(int i :a){
-            arr.add(i);
+        for (int i=0;i<b.length;i++) {
+            set.add(b[i]);
         }
-        Collections.sort(arr);
-        
-        return arr.get(k-1);
+
+
+Collections.sort(set);
+
+return set.get(k-1);
     }
 }
