1c69077b489f6816693458185c92db00fa9de434
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2026-01-01 13:06:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Intersection in Y Shaped Lists/intersection-in-y-shaped-lists.java
+++ Difficulty: Medium/Intersection in Y Shaped Lists/intersection-in-y-shaped-lists.java
@@ -1,7 +1,4 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
+/*
 class Node {
     int data;
     Node next;
@@ -11,126 +8,20 @@ class Node {
         next = null;
     }
 }
+*/
 
-class LinkedList_Intersection {
-    Node head = null;
-    Node tail = null;
+class Solution {
+    public Node intersectPoint(Node head1, Node head2) {
+        if (head1 == null || head2 == null) return null;
 
-    public void addToTheLast(Node node) {
+        Node p1 = head1;
+        Node p2 = head2;
 
-        if (head == null) {
-            head = node;
-            tail = head;
-        } else {
-            // Node temp = head;
-            // while (temp.next != null)
-            // temp = temp.next;
-
-            // temp.next = node;
-            tail.next = node;
-            tail = node;
+        while (p1 != p2) {
+            p1 = (p1 == null) ? head2 : p1.next;
+            p2 = (p2 == null) ? head1 : p2.next;
         }
-    }
 
-    /* Function to print linked list */
-    void printList() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
-        System.out.println();
-    }
-
-    /* Driver program to test above functions */
-    public static void main(String args[]) throws IOException {
-
-        /* Constructed Linked List is 1->2->3->4->5->6->
-           7->8->8->9->null */
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t > 0) {
-
-            LinkedList_Intersection llist1 = new LinkedList_Intersection();
-            LinkedList_Intersection llist2 = new LinkedList_Intersection();
-            LinkedList_Intersection llist3 = new LinkedList_Intersection();
-
-            String str[] = read.readLine().trim().split(" ");
-            int n1 = str.length;
-            Node head1 = new Node(Integer.parseInt(str[0]));
-
-            Node tail1 = head1;
-
-            for (int i = 1; i < n1; i++) {
-                int a = Integer.parseInt(str[i]);
-                tail1.next = (new Node(a));
-                tail1 = tail1.next;
-            }
-
-            str = read.readLine().trim().split(" ");
-            Node head2 = new Node(Integer.parseInt(str[0]));
-            Node tail2 = head2;
-            int n2 = str.length;
-            for (int i = 1; i < n2; i++) {
-                int b = Integer.parseInt(str[i]);
-                tail2.next = (new Node(b));
-                tail2 = tail2.next;
-            }
-
-            str = read.readLine().trim().split(" ");
-            int n3 = str.length;
-            if (n3 > 0) {
-
-                Node head3 = new Node(Integer.parseInt(str[0]));
-                tail1.next = head3;
-                tail2.next = head3;
-                Node tail3 = head3;
-                for (int i = 1; i < n3; i++) {
-                    int c = Integer.parseInt(str[i]);
-                    tail3.next = (new Node(c));
-                    tail3 = tail3.next;
-                }
-            }
-            Intersect obj = new Intersect();
-            Node intersectNode =
-                obj.intersectPoint(head1, head2); // Get the intersection node
-            System.out.println(intersectNode.data);
-            System.out.println("~");
-            t--;
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-/* Node of a linked list
- class Node {
-   int data;
-    Node next;
-    Node(int d)  { data = d;  next = null; }
-}
- Linked List class
-class LinkedList
-{
-    Node head;  // head of list
-}*/
-
-class Intersect {
-    // Function to find intersection point in Y shaped Linked Lists.
-    static Node intersectPoint(Node head1, Node head2) {
-        // code here
-        HashSet<Node> set = new HashSet<>();
-        Node temp = head1;
-        while(temp!= null) {
-            set.add(temp);
-            temp = temp.next;
-        }
-        temp = head2;
-        while(temp != null) {
-            if(set.contains(temp)) return temp;
-            temp = temp.next;
-        }
-        return null;
+        return p1; 
     }
 }
