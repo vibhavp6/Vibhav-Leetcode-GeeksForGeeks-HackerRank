e939a306d5ff5151e9446e352b83489e8f69e0a1
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2026-01-07 10:52:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count distinct elements in every window/count-distinct-elements-in-every-window.java
+++ Difficulty: Medium/Count distinct elements in every window/count-distinct-elements-in-every-window.java
@@ -1,58 +1,34 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-import java.util.HashMap;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            ArrayList<Integer> array = new ArrayList<>();
-
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            int k = Integer.parseInt(br.readLine());
-
-            ArrayList<Integer> ans = new Solution().countDistinct(arr, k);
-
-            for (Integer val : ans) System.out.print(val + " ");
-            System.out.println();
-
-            t--;
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     ArrayList<Integer> countDistinct(int arr[], int k) {
         // code here
-         ArrayList<Integer> ans = new ArrayList<>();
-         HashMap<Integer,Integer> mpp = new HashMap<>();
-         int st = 0;
-         for(int i = 0; i<arr.length; i++){
-             mpp.put(arr[i] , mpp.getOrDefault(arr[i] , 0 ) +1);
-             if(i-st+1 == k) {
-                 ans.add(mpp.size());
-                 mpp.put(arr[st] , mpp.get(arr[st]) -1);
-                 if(mpp.get(arr[st]) == 0){
-                     mpp.remove(arr[st]);
-                 }
-                 st++;
-             }
-         }
-         return ans;
+        ArrayList<Integer> res = new ArrayList<>();
+        int i = 0, n = arr.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for(int j=0; j<n; j++){
+            map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
+            
+            if(j - i + 1 == k){
+                res.add(map.size());
+                if(map.containsKey(arr[i]) && map.get(arr[i]) > 1){
+                    map.put(arr[i], map.get(arr[i]) - 1);
+                }else{
+                    map.remove(arr[i]);
+                }
+                i++;
+            }
+            
+        }
+        
+        return res;
+        
+    }
+    private int count(int[] arr, int i, int j){
+        Map<Integer, Integer> map = new HashMap<>();
+        while(i <= j){
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            i++;
+        }
+        return map.values().size();
     }
 }
