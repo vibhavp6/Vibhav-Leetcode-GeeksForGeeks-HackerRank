e3c76f7156996a0ded04849151f8e4ba3846d008
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2026-02-03 12:48:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.java
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.java
@@ -1,47 +1,28 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String arr[] = br.readLine().split(" ");
-            int prices[] = new int[arr.length];
-
-            for (int i = 0; i < arr.length; i++) {
-                prices[i] = Integer.parseInt(arr[i]);
-            }
-            Solution obj = new Solution();
-            int res = obj.maximumProfit(prices);
-            System.out.println(res);
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    public int maximumProfit(int prices[]) {
+    public int maxProfit(int[] prices) {
         // Code here
-        int n = prices.length;
-        int buy = Integer.MAX_VALUE;
-        int profit = 0;
-        for(int i = 1;i<n;i++)
-        {       
-                buy = Math.min(buy,prices[i-1]);
-                profit = Math.max(profit,prices[i]- buy);
-                
-                
+        // Multiple Transactions Allowed
+        // 
+        // int profit = 0;
+        // for(int i = 1;i < prices.length;i++){
+        //     if(prices[i - 1] < prices[i])
+        //         profit += (prices[i] - prices[i - 1]);
+        // }
+        // return profit;
+        
+        
+        //At most one transaction allowed
+        
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices){
+            if (price < minPrice)
+                minPrice = price; // best buy so far
+            else
+                maxProfit = Math.max(maxProfit, price - minPrice); 
+                // best sell so far
         }
-        return profit;
+        return maxProfit;
     }
 }
-
