cbc7b1af7cbe25052d5dece3dfb5fadabba77c60
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2026-02-05 17:22:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.java
+++ Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.java
@@ -1,28 +1,27 @@
 class Solution {
     public int maxOnes(int arr[], int k) {
         // code here
-        int ans = 0;  
-        int zero = 0; 
-        int left = 0; 
+        int left = 0, maxLen = 0, zeros = 0;
 
-        // Iterate through the array with the right pointer
         for (int right = 0; right < arr.length; right++) {
-            // If we encounter a zero, increment zero
+            // Count zero if found
             if (arr[right] == 0) {
-                zero++;
+                zeros++;
             }
 
-            while (zero> k) {
+            
+            while (zeros > k) {
                 if (arr[left] == 0) {
-                    zero--;
+                    zeros--;
                 }
                 left++;
             }
 
-            ans = Math.max(ans, right - left + 1);
+           
+            maxLen = Math.max(maxLen, right - left + 1);
         }
 
-        return ans;
-        
+        return maxLen;
     }
+
 }
