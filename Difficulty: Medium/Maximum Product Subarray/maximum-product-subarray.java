2090aeec19b57113d4c051127e4616089afdc293
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2026-02-08 11:45:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
@@ -1,40 +1,20 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        while (tc-- > 0) {
-            String[] inputLine = br.readLine().split(" ");
-            int n = inputLine.length;
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-            System.out.println(new Solution().maxProduct(arr));
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    // Function to find maximum product subarray
     int maxProduct(int[] arr) {
         // code here
-        int ans = Integer.MIN_VALUE;
-        for (int i = 0; i<arr.length; i++) {
-            int sum = 1;
-            for  (int j = i ; j<arr.length; j++) {
-                sum *= arr[j];
-                ans = Math.max(ans,sum);
-            }
-        }
+        int maximumProduct = Integer.MIN_VALUE;
+        int prefix = 1;
+        int suffix = 1;
+        int n = arr.length;
         
-        return ans;
+        for(int i = 0 ; i < n ; i++){
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+            
+            prefix = prefix * arr[i];
+            suffix = suffix * arr[n - i - 1];
+            
+            maximumProduct = Math.max(maximumProduct , Math.max(prefix , suffix));
+        }
+        return maximumProduct;
     }
 }
