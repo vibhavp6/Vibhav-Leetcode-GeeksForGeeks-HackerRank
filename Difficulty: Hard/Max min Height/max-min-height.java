f7a42d08022f927e8648226e7d0c1988bcaba82d
Vibhav Pandey <124075000+vibhavp6@users.noreply.github.com>
2026-02-12 18:10:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max min Height/max-min-height.java
+++ Difficulty: Hard/Max min Height/max-min-height.java
@@ -1,41 +1,56 @@
 class Solution {
     public int maxMinHeight(int[] arr, int k, int w) {
-        // code here
         int n = arr.length;
-        int low = Arrays.stream(arr).min().getAsInt();
-        int high = low + k;
-        int result = low;
         
-        while(low <= high){
-            int mid = (low + high) / 2;
-            if(canAchieve(arr, k, w, mid)){
-                result = mid;
-                low = mid + 1;
-            }else{
+        int low = Integer.MAX_VALUE;
+        int high = Integer.MIN_VALUE;
+        
+        for (int num : arr) {
+            low = Math.min(low, num);
+            high = Math.max(high, num);
+        }
+        
+        high = high + k; // maximum possible height
+        
+        int ans = low;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (canAchieve(arr, n, k, w, mid)) {
+                ans = mid;
+                low = mid + 1;   // try higher minimum
+            } else {
                 high = mid - 1;
             }
         }
         
-        return result;
+        return ans;
     }
     
-    private boolean canAchieve(int[] arr, int k, int w, int target){
-        int n = arr.length;
-        int[] water = new int[n + 1];
-        int ops = 0, currWater = 0;
+    private boolean canAchieve(int[] arr, int n, int k, int w, int target) {
+        long[] diff = new long[n + 1];
+        long operations = 0;
+        long currentWater = 0;
         
-        for(int i=0; i<n; i++){
-            currWater += water[i];
-            int height = arr[i] + currWater;
-            if(height < target){
-                int need = target - height;
-                ops += need;
-                if(ops > k) return false;
-                currWater += need;
-                if(i + w < n) water[i + w] -= need;
+        for (int i = 0; i < n; i++) {
+            currentWater += diff[i];
+            
+            long currentHeight = arr[i] + currentWater;
+            
+            if (currentHeight < target) {
+                long need = target - currentHeight;
+                operations += need;
+                
+                if (operations > k) return false;
+                
+                currentWater += need;
+                
+                if (i + w < diff.length)
+                    diff[i + w] -= need;
             }
         }
         
-        return true;
+        return operations <= k;
     }
 }
